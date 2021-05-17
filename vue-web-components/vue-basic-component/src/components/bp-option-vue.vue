157482b9cf24d54d1d4eb006d7284ea77fd75413
yanjie <yj873891018@163.com>
2021-05-17 11:28:10 +0800

    添加bp-option-vue bp-select-vue

---
+++ vue-web-components/vue-basic-component/src/components/bp-option-vue.vue
@@ -0,0 +1,82 @@
+<template>
+    <li class="bp-option" :disabled="disabled" @click="close" :class="{'option-active': isChoosed}">
+        <img svg-inline :class="iconClass" :src="src" alt="" v-if="src"/>
+        <div style="display: flex;flex-direction: column;">
+            <span>{{text}}</span>
+            <div>
+                <slot></slot>
+            </div>
+        </div>
+    </li>
+</template>
+<script>
+export default {
+    data: function() {
+        return {
+            disabled: false
+        }
+    },
+    props: {
+        text: String,
+        choosedValue: String,
+        iconClass: {
+            default: "svg-icon",
+            type: String
+        },
+        src: String
+    },
+    computed: {
+        isChoosed() {
+            return this.text === this.choosedValue
+        }
+    },
+    methods: {
+        close() {
+            this.$parent.$data.show = false
+            this.$emit("onClick", this.text)
+        }
+    }
+}
+</script>
+<style lang=scss scoped>
+    $color-neutrals-n000: #ffffff;
+    $color-neutrals-n400: rgba(#091e42,0.71);
+    .svg-icon {
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+        margin-right: 4px;
+    }
+    svg:focus {
+        outline: none;
+    }
+    .option-active {
+        color: $color-neutrals-n000;
+        background-color: $color-neutrals-n400;
+    }
+    .option-active .icon path {
+        fill: $color-neutrals-n000;
+    }
+    .bp-option {
+        height: 32px;
+        width: 100%;
+        margin-left: 8;
+        margin-right: 8;
+        margin-top: 8;
+        margin-bottom: 8;
+        padding-left: 8;
+        padding-right: 8;
+        padding-top: 8;
+        padding-bottom: 8;
+        background: transparent;
+        color: #344563;
+        display: flex;
+        padding: 0 16px;
+        align-items: center;
+    }
+    .bp-option:hover {
+        background-color: #EBECF0;
+    }
+</style>
