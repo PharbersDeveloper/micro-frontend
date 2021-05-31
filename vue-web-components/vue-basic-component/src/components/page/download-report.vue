5aea8f5a22f73111aea0c104e57081902556ffa2
wodelu <1058744843@qq.com>
2021-05-31 18:23:34 +0800

    新页面

---
+++ vue-web-components/vue-basic-component/src/components/page/download-report.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="">
+       
+    </div>
+</template>
+<script>
+import bpText from '../bp-text'
+import bpImg from '../bp-img'
+import bpButton from '../bp-button'
+export default {
+    name: "",
+    components: {
+        bpText,
+        bpImg,
+        bpButton
+    },
+    data() {
+        return {
+            language: '中文', // 中英切换
+            windowWidth: document.documentElement.clientWidth,
+            windowHeight: document.documentElement.clientHeight,
+            response: false,
+            responseMini: false,
+            translation_basedata: {
+                cn: {
+
+                },
+                en: {
+
+                }
+            }
+        }
+    },
+    computed: {
+        translation_data: function() {
+            if (this.language === '中文') {
+                return this.translation_basedata.cn
+            } else if (this.language === 'English') {
+                return this.translation_basedata.en
+            }
+        }
+    },
+    watch: {
+        windowWidth(val) {
+            if(val < 1100) {
+                this.response = true
+            } else {
+                this.response = false
+            }
+            if(val <= 549) {
+                this.responseMini = true
+            } else {
+                this.responseMini = false
+            }
+        },
+        windowHeight(val) {
+            console.log('height', val, this.windowHeight)
+        }
+    },
+    created() {
+        window.addEventListener('beforeunload', e => {
+            window.scrollTo(0,0)
+        });
+        let originalSetItem = localStorage.setItem,
+            lang = window.localStorage.getItem('lang')
+        if (!lang) {
+            lang = navigator.language || navigator.userLanguage
+            if (lang === 'English') {
+                localStorage.setItem('lang', 'English')
+            } else {
+                localStorage.setItem('lang', '中文')
+            }
+        } else {
+            this.language = lang
+        }
+        localStorage.setItem = function(key,newValue){
+            let event = new Event("setItemEvent");
+            event.key = key;
+            event.newValue = newValue;
+            window.dispatchEvent(event);
+            originalSetItem.apply(this,arguments);
+        }
+    },
+    mounted() {
+        const that = this;
+        if(this.windowWidth < 1100) {
+            this.response = true
+        } else {
+            this.response = false
+        }
+        if(this.windowWidth <= 549) {
+            this.responseMini = true
+        } else {
+            this.responseMini = false
+        }
+        window.onresize = () => {
+            return (() => {
+                window.windowWidth = document.body.clientWidth;
+                window.windowHeight = document.body.clientHeight;
+                that.windowWidth = window.windowWidth;
+                that.windowHeight = window.windowHeight;
+            })()
+        }
+        window.addEventListener('setItemEvent', function(e) {
+            that.language = e.newValue
+        })
+    },
+    methods: {
+        toHome() {
+            this.$emit('linkToPage', 'home')
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    * {
+        letter-spacing: .4px;
+        line-height: 1.6;
+        box-sizing: border-box;
+    }
+</style>
