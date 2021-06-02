5829783de6977ca2be60f58a3eda3678af56cf82
wodelu <1058744843@qq.com>
2021-06-02 15:14:08 +0800

    download-report

--- pharbers-web/app/routes/download-report.js
+++ pharbers-web/app/routes/download-report.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp'
 
 export default class DownloadReportRoute extends Route {
-    model(params) {
-        console.log(params);
+    @service store;
+    beforeModel() {
+        this.controllerFor('application').set('inverse', false)
+    }
+    model() {
+        let lang = localStorage.getItem('lang')
+        if (lang === "中文") {
+            lang = 1
+        } else {
+            lang = 0
+        }
+        const reportsData = this.store.findRecord("report", "MzsAreGknrYrBiSax9Xk")
+        return hash({
+            data: reportsData
+        })
     }
 }
