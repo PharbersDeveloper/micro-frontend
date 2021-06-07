47fb0c5052406f422683fba3e018ddffb0f89f16
yanjie <yj873891018@163.com>
2021-06-07 15:40:15 +0800

    修改了adapter，现在findRecord可以添加include了

--- pharbers-web/app/routes/boyunhui.js
+++ pharbers-web/app/routes/boyunhui.js
@@ -1,13 +1,16 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class BoyunhuiRoute extends Route {
     @service store;
 
     model(params) {
-        const id = params.boyunhui_id,
-            activityList = this.store.findRecord("activity", id, {include: "gallery"}),
-            reportList = this.store.query( "report",  { 'sort': "-date" } )
+        const id = params.boyunhui_id
+        const activityList = this.store.findRecord('activity', id, {include: 'logo,gallery,attachments,agendas,agendas.agendas,agendas.agendas.speakers,agendas.agendas.speakers.avatar'})
 
+        return hash({
+            data: activityList
+        })
     }
 }
