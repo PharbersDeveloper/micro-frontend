c0794bd3cff6d065682e80f30a4b520b18bc034a
yanjie <yj873891018@163.com>
2021-06-22 18:46:44 +0800

    样式基本完成

--- vue-web-components/vue-basic-component/src/components/bp-dag.vue
+++ vue-web-components/vue-basic-component/src/components/bp-dag.vue
@@ -36,27 +36,49 @@ export default {
         addNodes(nodeData, g, hasParent) {
             nodeData.forEach( item => {
                 let shape = ""
-                let style = "stroke-dasharray:5 2;fill:#fff;stroke:#000;cursor: pointer"
+                let style = "fill:#fff;stroke:#000;cursor: pointer"
+                let labelStyle,width,labelType
 
                 switch (item.type) {
-                case "Task":
-                case "Pass":
-                    shape = "rect"
-                    break;
                 case "Start":
+                    shape = "rect"
+                    style = "fill: #7163C5;cursor: pointer;"
+                    labelStyle = "font-family: SFProText-Regular;font-size: 24px;fill: #fff;color: #fff;cursor: pointer;"
+                    width = (120-20)
+                    break;
                 case "End":
-                    shape = "circle"
-                    style = "fill:#ffda75;stroke:#000;cursor: pointer"
+                    shape = "rect"
+                    style = "fill: #23A959;cursor: pointer;"
+                    labelStyle = "font-family: SFProText-Regular;font-size: 24px;fill: #fff;color: #fff;cursor: pointer;"
+                    width = (120-20)
                     break;
                 default:
                     shape = "rect"
-                    break;
+                    labelType = "html"
                 }
-                g.setNode( item.id, {
+                
+                let params = {
                     label: item.label,
+                    height: (48-20),
                     shape: shape,
-                    style: style
-                } )
+                    style: style,
+                    rx: 2,
+                    ry: 2,
+                    class: 'test_dag_status'  //可以根据不同的状态给不同的class以变化不同的边框颜色
+                }
+
+                if (labelStyle) {
+                    params['labelStyle'] = labelStyle
+                }
+
+                if (width) {
+                    params['width'] = width
+                }
+                if (labelType) {
+                    params['labelType'] = labelType
+                    params.label = '<div class=icon_dag_container><span class=icon_dag></span>'+`<span style="height:100%;display: inline-block">${params.label}</span></div>`
+                }
+                g.setNode( item.id, params )
             } )
             // 添加map的父元素
             if (hasParent) {
@@ -74,7 +96,8 @@ export default {
         addEdges(edgeData, g) {
             edgeData.forEach( item => {
                 g.setEdge( item.source, item.target, {
-                    style: "fill:#fff;stroke:#333;stroke-width:1.5px;cursor: pointer"
+                    style: "fill:#fff;stroke:#333;stroke-width:1.5px;cursor: pointer",
+                    curve: d3.curveBasis
                 } )
             } )
         },
@@ -112,21 +135,21 @@ export default {
                     })
                     nodeData.push({
                         id: this.guid(),
-                        label: "Start",
+                        label: "START",
                         type: "Start",
                         next: parallel_start_next
                     })
                 } else if (dag.States[firstStep].Type === "Map") {
                     nodeData.push({
                         id: this.guid(),
-                        label: "Start",
+                        label: "START",
                         type: "Start",
                         next: dag.States[firstStep].Iterator.StartAt
                     })
                 } else {
                     nodeData.push({
                         id: this.guid(),
-                        label: "Start",
+                        label: "START",
                         type: "Start",
                         next: firstStep
                     })
@@ -135,13 +158,13 @@ export default {
                 // States中包含了状态机的所有状态
                 // 添加state节点数据
                 for (let state in dag.States) {
-                    let next = dag.States[state].Next ? dag.States[state].Next : "End",
+                    let next = dag.States[state].Next ? dag.States[state].Next : "END",
                         catchNext = [],
                         catchNextLength = dag.States[state].Catch ? dag.States[state].Catch.length : 0,
                         type = dag.States[state].Type
 
                     // 处理next的type为Map情况下的节点数据(普通情况)
-                    if (next !== "End" && type != "Parallel" && dag.States[next].Type === "Map") {
+                    if (next !== "END" && type != "Parallel" && dag.States[next].Type === "Map") {
                         nodeData.push({
                             id: this.guid(),
                             label: state,
@@ -152,7 +175,7 @@ export default {
                         continue
                     }
                     // 处理next的type为parallel时的节点数据(普通情况)
-                    if (next !== "End" && type != "Map" && dag.States[next].Type === "Parallel") {
+                    if (next !== "END" && type != "Map" && dag.States[next].Type === "Parallel") {
                         let parallel_next = []
                         dag.States[next].Branches.forEach(branch => {
                             parallel_next.push(branch.StartAt)
@@ -249,7 +272,7 @@ export default {
                 // 添加end节点
                 nodeData.push({
                     id: this.guid(),
-                    label: "End",
+                    label: "END",
                     type: "End",
                     next: ""
                 })
@@ -309,6 +332,8 @@ export default {
 
                 window.onload = function() {
                     let svg = d3.select( document.getElementsByTagName("pharbers-bp-dag")[0].shadowRoot ).select("#svg-canvas")
+                    // let svg = d3.select("#svg-canvas")
+
                     // 绘图的容器
                     let svgGroup = svg.append( "g" )
                     // 开始渲染
@@ -333,6 +358,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+    .icon_dag_container {
+        display: flex;
+        align-items: center;
+    }
 
+    .icon_dag_container .icon_dag {
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        background: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M10.87 10.18c0 .2-.205.401-.412.401a.438.438 0 0 1-.413-.4c0-.201.207-.403.413-.403.207 0 .413.101.413.402h-.001zm-1.856 0c0 .802.619 1.404 1.444 1.404s1.444-.602 1.444-1.404c0-.803-.62-1.504-1.444-1.504-.825 0-1.444.701-1.444 1.504zm-1.65-1.404c0-.1 0-.1.103-.1l-.104.1zm8.148-.601a17.816 17.816 0 0 0-5.467-.602h-.62c.723-1.003 1.446-1.906 2.167-2.607 1.135-1.003 1.65-1.103 1.858-1.003.206.1.413 1.003-.103 2.707l1.03.301c.62-2.005.517-3.409-.308-3.81-.515-.302-1.444-.302-3.094 1.103-1.031.802-1.96 2.005-2.888 3.31-.413.1-.723.1-1.135.2-.722-2.006-.722-3.21-.412-3.41.206-.1 1.03.1 2.372 1.404l.722-.702C8.086 3.462 6.848 2.96 6.023 3.462c-1.033.501-.826 2.306-.103 4.412-.517.1-.93.2-1.341.3C2.413 8.777 2 9.579 2 10.18c0 .903 1.135 1.704 3.197 2.206l.206-1.002c-1.65-.402-2.372-.903-2.372-1.103 0-.201.413-.602 1.857-1.104.413-.1.928-.2 1.445-.3.102.3.309.7.515 1.103-.62 1.203-1.134 2.406-1.34 3.51-.517 2.105 0 2.907.514 3.208.207.1.413.2.62.2.72 0 1.753-.6 2.888-1.704l-.722-.7c-1.238 1.302-2.063 1.502-2.27 1.402-.206-.1-.412-.602 0-2.106.104-.902.413-1.805.825-2.808.104.201.207.301.31.502 1.032 1.705 2.166 3.209 3.3 4.211C12.109 16.698 12.83 17 13.449 17c.31 0 .516-.101.619-.202.825-.5.928-1.805.31-3.71a17.847 17.847 0 0 0-1.444-3.31l-.825.503c.618 1.103 1.03 2.206 1.34 3.108.516 1.705.31 2.507.104 2.607-.207.1-.722 0-1.857-1.003-1.031-.902-2.166-2.406-3.094-4.01-.31-.403-.517-.702-.62-1.104.104-.301.31-.601.516-.902.104-.1.207-.302.207-.401h1.34c1.96 0 3.817.2 5.158.601 1.444.402 1.857.903 1.857 1.104 0 .3-.62.802-2.476 1.203l.206 1.003c2.166-.502 3.198-1.305 3.198-2.206.103-.703-.413-1.505-2.476-2.106z' fill='%2357565F' fill-rule='evenodd'/%3E%3C/svg%3E") no-repeat center/100% !important;
+    }
 </style>
