b022f28fedce93079b6ee888c815f3bfec7499ba
yanjie <yj873891018@163.com>
2021-06-29 18:21:34 +0800

    my data的组件，暂时提交一下

---
+++ vue-web-components/vue-catelog-component/src/components/panel/my-data-table.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="tabs">
+            <span :class="curTab === 0 ? 'label_button_theme': 'btn_secondary_initial'" class="cursor-pointer mr-3">Mine</span>
+            <span>Subscribed</span>
+        </div>
+        <div class="data-main-container">
+            <div v-if="haveData" class="myData-mine">
+
+            </div>
+
+            <div v-else>
+
+            </div>
+        </div>
+    </div>
+    
+</template>
+<script>
+import bpPagination from '../bp-pagination.vue'
+
+export default {
+    components: {
+        bpPagination
+    },
+    data() {
+        return {
+            haveData: true,
+            curTab: 0
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .cursor-pointer {
+        cursor: pointer;
+    }
+    .mr-3 {
+        margin-right: 16px;
+    }
+
+    .label_button_theme {
+        font-family: SFProText-Medium;
+        font-size: 14px;
+        letter-spacing: 0;
+        line-height: 20px;
+        color: #7163C5;
+    }
+
+    .btn_secondary_initial {
+        font-family: SFProText-Medium;
+        font-size: 14px;
+        letter-spacing: 0;
+        line-height: 20px;
+        color: #57565F;
+    }
+</style>
--- vue-web-components/vue-catelog-component/src/router.js
+++ vue-web-components/vue-catelog-component/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import test from './views/test.vue'
+import myDataTable from './components/panel/my-data-table.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'test',
             component: test
+        },
+        {
+            path: '/my-data-table',
+            name: 'myDataTable',
+            component: myDataTable
         }
     ]
 })
