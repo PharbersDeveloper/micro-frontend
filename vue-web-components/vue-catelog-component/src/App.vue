009b55afbe661ff3baeea93120c8189ac680cd6c
yanjie <yj873891018@163.com>
2021-07-09 17:23:05 +0800

    mine table的基本样式完成，按键功能未完成，底部换页符待添加

--- vue-web-components/vue-catelog-component/src/App.vue
+++ vue-web-components/vue-catelog-component/src/App.vue
@@ -9,7 +9,7 @@
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         color: #2c3e50;
-        min-height: 100vh;
+        height: 100vh;
     }
     body {
         margin: 0;
