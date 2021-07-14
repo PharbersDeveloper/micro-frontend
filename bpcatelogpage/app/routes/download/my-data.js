d95b2e1e9ff1e5664af3f40770e37943a4918396
yanjie <yj873891018@163.com>
2021-07-14 18:19:23 +0800

    ember中添加model、adapter、serializer、environment等，表格数据请求成功
    还需要在vue中将写死的数据改为ember中的数据，事件的params还需要进行调整

---
+++ bpcatelogpage/app/routes/download/my-data.js
@@ -0,0 +1,52 @@
+import Route from '@ember/routing/route';
+import { inject as service } from "@ember/service"
+import RSVP from 'rsvp';
+
+export default class DownloadMyDataRoute extends Route {
+    @service store
+    @service cookies
+    
+    queryParams = {
+        tab: {
+            refreshModel: true
+        },
+        sort: {
+            refreshModel: true,
+        },
+        page: {
+            refreshModel: true
+        }
+    }
+
+    async model( params ) {
+        const limit = 10
+        let tab = params.tab || "mine"
+		let page = parseInt( params.page, 10 )
+		let sortType = params.sort || '-created'
+        if ( isNaN( page ) ) {
+			page = 0
+		}
+
+        let files = this.store.query( "asset", { "filter[type]": "file", "filter[owner]": this.cookies.read('account_id'), "page[limit]": limit, "page[offset]": page * limit, sort: sortType } )
+            // .then()
+            // .catch(err => {
+            //     // this.toast.error( "", this.get('intl').t('permission.permissionDenied'), this.toastOptions )
+            //     // return {
+            //     //     meta: {
+            //     //         count: 1
+            //     //     }
+            //     // }
+            // })
+
+        if ( files.length === 0 && page !== 0 ) {
+            this.transitionTo( `/download/my-data?tab=${tab}&page=${page}sort=${sortType}`)
+        }
+
+        return RSVP.hash({
+            files: files.then(file => file.filter(it => it)),
+            tab: tab,
+            page: page,
+            sort: sortType
+        })
+    }
+}
