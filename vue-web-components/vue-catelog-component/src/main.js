540459ba4f022ef5565ecc1220e10f3d5f9bc153
yanjie <yj873891018@163.com>
2021-07-15 10:50:27 +0800

    install vue-cookies

--- vue-web-components/vue-catelog-component/src/main.js
+++ vue-web-components/vue-catelog-component/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 // import wrap from '@vue/web-component-wrapper';
 import App from './App.vue'
 import router from './router'
+import VueCookies from 'vue-cookies'
 
+Vue.use(VueCookies)
 Vue.config.productionTip = false
 
 new Vue({
