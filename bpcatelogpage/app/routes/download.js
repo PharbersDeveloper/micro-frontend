2e93329dc57c6423c9a19e4ec76fc5ab40980433
wodelu <1058744843@qq.com>
2021-07-19 16:17:15 +0800

    菜单栏展示userInfo

--- bpcatelogpage/app/routes/download.js
+++ bpcatelogpage/app/routes/download.js
@@ -1,9 +1,54 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { hash } from 'rsvp';
+import RSVP from 'rsvp';
 import ENV from 'bpcatelogpage/config/environment';
 
 export default class DownloadRoute extends Route {
-	@service intl
 	@service cookies
+	@service ajax
+	@service store
+	namespace = ENV.namespace
+
+	beforeModel() {
+		let cookies = this.get( "cookies" )
+
+		if ( !cookies.read( "access_token" ) ) {
+			this.transitionTo( "index" )
+		}
+	}
+
+	async model() {
+		//请求user的数据
+		const accessToken = this.cookies.read( "access_token" )
+		const applicationAdapter = this.get( "store" ).adapterFor( "application" )
+		applicationAdapter.toggleProperty( "oauthRequest" )
+		applicationAdapter.set("getUserInfo", 1)
+		applicationAdapter.set("userAuthorization", accessToken)
+		let allUserData
+		let userData = await this.store.findRecord( "account", this.cookies.read('account_id') )
+		//请求employer的数据
+		let employerId = userData.belongsTo('employer').id()
+		applicationAdapter.set("partner",1)
+		let employerData = await this.store.findRecord( "partner", employerId )
+		
+		const options = {
+			domain: ".pharbers.com",
+			path: "/",
+			maxAge: this.cookies.read( "expires_in" )
+		}
+
+		this.cookies.write( "account_id", userData.id, options )
+		this.cookies.write( "user_email", userData.email, options )
+
+		applicationAdapter.toggleProperty( "oauthRequest" )
+		applicationAdapter.set("getUserInfo", 1)
+		applicationAdapter.set("userAuthorization", accessToken)
+		applicationAdapter.set('needUserData', allUserData)
+		return RSVP.hash( {
+			personalData: userData,
+			employerId: employerId,
+			employerData: employerData
+		} )
+	}
+
 }
