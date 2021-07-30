eb920966636927b47dfd3fe390495fb462ce068d
wodelu <1058744843@qq.com>
2021-07-30 17:05:32 +0800

    临时提交

--- bpcatelogpage/app/components/my-data.js
+++ bpcatelogpage/app/components/my-data.js
@@ -47,7 +47,8 @@ export default class MyDataComponent extends Component {
 				}
 				//上传文件 
 				else if(e.detail[0].args.param.name == "uploadFile") {
-					$('#my-file').click()
+					// $('#my-file').click()
+					this.uploadFiles(e.detail[0].args, e.target)
 				}
 				break
 			case "closeToast":
@@ -59,15 +60,15 @@ export default class MyDataComponent extends Component {
 	}
 
 	@action
-	async uploadFiles(title, event) {
+	async uploadFiles(event,target) {
+		debugger
 		//初始化变量
-		this.uploadToastBorder = "blue"
-		this.uploadTextStatus = "正在上传"
-		this.uploadText = ""
-		this.closeuploadToast = "0" //显示上传弹框
-
+		target.allData.uploadToastBorder = "blue"
+		target.allData.uploadTextStatus = "正在上传"
+		target.allData.uploadText = ""
+		target.allData.closeuploadToast = "0" //显示上传弹框
 		//禁用上传文件input
-		let dom = $( event.target )
+		let dom = $( event.param.uploadEvent.target )
 		dom[0].disabled = true
 
 		function guid() {
@@ -85,14 +86,15 @@ export default class MyDataComponent extends Component {
 		const traceId = guid()
 		let uploadMessage = {}
 		uploadMessage.accountId = this.cookies.read( "account_id" )
-		uploadMessage.file = document.getElementById( "my-file" ).files[0]
-
+		uploadMessage.file = event.element.$refs.inputUpload.files[0]
+		// uploadMessage.file = document.getElementById( "my-file" ).files[0]
+		console.log(111111)
 		/**
 		 * 2. upload file to OSS
 		 */
 		let that = this
-		this.showProgress = '1' //显示上传进度
-		that.uploadFileSize = uploadMessage.file.size  // 上传文件总大小
+		target.allData.showProgress = '1' //显示上传进度
+		target.allData.uploadFileSize = uploadMessage.file.size  // 上传文件总大小
 
 		// aws s3 upload
 		const accountId = this.cookies.read( "account_id" )
@@ -110,10 +112,10 @@ export default class MyDataComponent extends Component {
 			await s3Client
 				.upload( uploadFileParams )
 				.on( "httpUploadProgress", function ( progress ) {
-					that.uploadLoadedSize = progress.loaded //实时进度
+					target.allData.uploadLoadedSize = progress.loaded //实时进度
 				} )
 				.promise()
-		
+				console.log(22222)
 			/**
 			 * 3. create file metadata for database
 			 */
@@ -140,35 +142,40 @@ export default class MyDataComponent extends Component {
 				description: "",
 				partners: that.args.model.employerId
 			}
+			console.log(33333)
 			applicationAdapter.set( "reqBody", fileBodyObj )
 			//数据库上传数据
 			await this.store
 				.createRecord( "asset", fileBodyObj )
 				.save()
-
+			console.log(44444)
 			that.router.transitionTo( "/" )
-			that.router.transitionTo( title.router.currentURL )
-			that.showProgress = '0'// 关闭上传进度条
-
+			that.router.transitionTo( that.router.currentURL )
+			target.allData.showProgress = '0'// 关闭上传进度条
+			console.log(55555)
 			//上传成功提示
-			that.uploadTextStatus = "上传成功"
-			that.uploadText = "在“我的数据”中查看结果"
-			that.uploadToastBorder = "green"
+			target.allData.uploadTextStatus = "上传成功"
+			target.allData.uploadText = "在“我的数据”中查看结果"
+			target.allData.uploadToastBorder = "green"
+			console.log(66666)
 		} catch ( e ) {
-			that.showProgress = '0' //关闭上传进度条
+			target.allData.showProgress = '0' //关闭上传进度条
 			//上传失败提示
-			that.uploadTextStatus = "上传失败" 
-			that.uploadText = ""
-			that.uploadToastBorder = "red"
+			target.allData.uploadTextStatus = "上传失败" 
+			target.allData.uploadText = ""
+			target.allData.uploadToastBorder = "red"
 		}
-
+		console.log(77777)
 		// 不管上传成功还是失败，都把按键的disabled取消，样式还原,loaded大小还原
 		dom[0].disabled = false
-		that.uploadLoadedSize = 0
-
+		target.allData.uploadLoadedSize = 0
+		console.log(8888)
 		// 不管上传成功还是失败，都把文件清空
-		let fileContainer = document.getElementById( "my-file" )
-		fileContainer.value = null
+		// let fileContainer = document.getElementById( "my-file" )
+		let fileContainer = dom[0]
+		fileContainer.files = null
+		fileContainer.value = ''
+		console.log(99999)
 	}
 
     @action
