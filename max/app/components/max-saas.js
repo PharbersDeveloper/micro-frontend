974b7d89b24db5a18d6f68506a03364fab9dc13c
wodelu <1058744843@qq.com>
2021-08-27 18:18:09 +0800

    解决第二次点击上传组件无反应问题

--- max/app/components/max-saas.js
+++ max/app/components/max-saas.js
@@ -116,8 +116,8 @@ export default class MaxSaasComponent extends Component {
             let fileVersion = s3FileUpload.Key.split("/").pop()
             // upload file tag
             let tagParam = {
-                Bucket: s3FileUpload.Bucket, 
-                Key: s3FileUpload.Key, 
+                Bucket: s3FileUpload.Bucket,
+                Key: s3FileUpload.Key,
                 Tagging: {
                     TagSet: [
                         {
@@ -133,7 +133,6 @@ export default class MaxSaasComponent extends Component {
                             Key: "provider", 
                             // Value: this.provider
                             Value: "pharbers"
-
                         },
                         {
                             Key: "date", 
@@ -224,160 +223,15 @@ export default class MaxSaasComponent extends Component {
 
     @action
     async uploadFiles(title, event) {
-        //初始化变量
-        this.uploadToastBorder = "blue"
-        this.uploadTextStatus = "正在上传"
-        this.uploadText = ""
-        this.closeuploadToast = "0" //显示上传弹框
-
         //禁用上传文件input
         let dom = $( event.target )
         dom[0].disabled = true
+        
+        let uploadFile = {}
+        uploadFile.file = document.getElementById( "my-file" ).files[0]
+        this.fileName = uploadFile.file.name //文件名称传入component
 
-        function guid() {
-            return "xxxxx-xxxx-4xxx-yxxx-xxxxx".replace( /[xy]/g, function ( c ) {
-                var r = Math.random() * 16 | 0,
-                    v = c === "x" ? r : r & 0x3 | 0x8
-
-                return v.toString( 16 )
-            } )
-        }
-
-        /**
-         * 1. 初始化上传流程
-         */
-        const traceId = guid()
-        let uploadMessage = {}
-        uploadMessage.accountId = this.cookies.read( "account_id" )
-        uploadMessage.file = document.getElementById( "my-file" ).files[0]
-        this.fileName = uploadMessage.file.name //文件名称传入component
-
-        // /**
-        //  * 2. upload file to OSS
-        //  */
-        // let that = this
-        // this.showProgress = '1' //显示上传进度
-        // that.uploadFileSize = uploadMessage.file.size  // 上传文件总大小
-
-        // // aws s3 upload
-        // const accountId = this.cookies.read( "account_id" )
-        // const bucketName = "ph-origin-files"
-        // const s3Client = this.awsService.get( "s3Client" )
-        // const fileKey = `user/${accountId}/${traceId}/${uploadMessage.file.name}`
-        // let uploadFileParams = {
-        //     Bucket: bucketName,
-        //     Key: fileKey.replace(/[(|)|（|）]/g, "_"),
-        //     Body: uploadMessage.file
-        // }
-
-        // try {
-        //     //S3 上传
-        //     let s3FileUpload = await s3Client
-        //         .upload( uploadFileParams )
-        //         .on( "httpUploadProgress", function ( progress ) {
-        //             that.uploadLoadedSize = progress.loaded //实时进度
-        //         } )
-        //         .promise()
-        //     let fileVersion = s3FileUpload.Key.split("/").pop()
-        //     // upload file tag
-        //     let tagParam = {
-        //         Bucket: s3FileUpload.Bucket, 
-        //         Key: s3FileUpload.Key, 
-        //         Tagging: {
-        //             TagSet: [
-        //                 {
-        //                     Key: "owner", 
-        //                     // Value: this.args.model.userData.name
-        //                     Value: "pharbers"
-        //                 },
-        //                 {
-        //                     Key: "application", 
-        //                     Value: "max"
-        //                 }, 
-        //                 {
-        //                     Key: "provider", 
-        //                     // Value: this.provider
-        //                     Value: "pharbers"
-
-        //                 },
-        //                 {
-        //                     Key: "date", 
-        //                     Value: this.uploadDate
-        //                 },{
-        //                     Key: "time", 
-        //                     Value: new Date().getTime().toString()
-        //                 },
-        //                 {
-        //                     Key: "version", 
-        //                     Value: "hdf_data_17"
-        //                 },
-        //                 {
-        //                     Key: "filename", 
-        //                     Value: fileVersion
-        //                 }
-        //             ]
-        //         }
-        //     }
-        //     s3Client.putObjectTagging(tagParam, function(err, data) {
-        //         if (err) console.log(err, err.stack); // an error occurred
-        //         else     console.log(data);           // successful response
-        //     });
-        //     /**
-        //      * 3. create file metadata for database
-        //      */
-
-        //     const applicationAdapter = this.store.adapterFor( "application" )
-        //     const fileBodyObj = {
-        //         name: uploadMessage.file.name.split( "." )[0],
-        //         owner: accountId,
-        //         extension: uploadMessage.file.name.split( "." )[1],
-        //         size: uploadMessage.file.size,
-        //         source: fileKey,
-        //         type: "file", // candidate: database, file, stream, application, mart, cube
-        //         accessibility: "",
-        //         version: "",
-        //         isNewVersion: true,
-        //         providers: [],
-        //         markets: [],
-        //         molecules: [],
-        //         dateCover: [],
-        //         geoCover: [],
-        //         labels: ["owner",this.args.model.userData.name, "application", "max", "provider", this.provider, "date", this.uploadDate, "time", new Date().getTime().toString(), "version", fileVersion],
-        //         created: new Date(),
-        //         modified: new Date(),
-        //         description: "",
-        //         partners: that.args.model.employerId
-        //     }
-        //     applicationAdapter.set( "reqBody", fileBodyObj )
-        //     //数据库上传数据
-        //     await this.store
-        //         .createRecord( "asset", fileBodyObj )
-        //         .save()
-
-        //     that.router.transitionTo( "/" )
-        //     that.router.transitionTo( title.router.currentURL )
-        //     that.showProgress = '0'// 关闭上传进度条
-
-        //     //上传成功提示
-        //     that.uploadTextStatus = "上传成功"
-        //     that.uploadText = "在“我的数据”中查看结果"
-        //     that.uploadToastBorder = "green"
-        // } catch ( e ) {
-        //     console.log(e)
-        //     that.showProgress = '0' //关闭上传进度条
-        //     //上传失败提示
-        //     that.uploadTextStatus = "上传失败" 
-        //     that.uploadText = ""
-        //     that.uploadToastBorder = "red"
-        // }
-
-        // // 不管上传成功还是失败，都把按键的disabled取消，样式还原,loaded大小还原
-        // dom[0].disabled = false
-        // that.uploadLoadedSize = 0
-
-        // // 不管上传成功还是失败，都把文件清空
-        // let fileContainer = document.getElementById( "my-file" )
-        // fileContainer.value = null
+        dom[0].disabled = false
     }
 
     @action
