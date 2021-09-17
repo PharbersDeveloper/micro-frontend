39225e0991a497b16884c36294279fb87770f45e
wodelu <1058744843@qq.com>
2021-09-17 17:36:53 +0800

    文件列表数据

--- max/app/routes/max-saas/import.js
+++ max/app/routes/max-saas/import.js
@@ -24,23 +24,18 @@ export default class MaxSaasImportRoute extends Route {
 			time = new Date(ym).getTime()
 		}
 		//文件列表显示
-		let assets = this.store.query("jobLog", { "filter[provider]": params.provider, "filter[time]": time, "filter[jobDesc]": "SUCCEEDED", "filter[jobCat]": "upload"})
-		// 文件列表状态显示
-		let mapperAssets = this.store.query("jobLog", { "filter[provider]": params.provider, "filter[time]": time, "filter[jobCat]": "mapper"})
-		// datacource
-		let stateUrl = "https://api.pharbers.com/phdatasource"
-		let options = {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				"Authorization": this.cookies.read( "access_token" ),
-				"Content-Type": "application/vnd.api+json",
-				"Accept": "application/vnd.api+json",
-			}
-		}
-		let datasource = fetch(stateUrl, options).then(res=>res.json())
-		//schemas
+		let assets = await this.store.query("jobLog", { "filter[provider]": params.provider, "filter[time]": time, "filter[jobDesc]": "SUCCEEDED", "filter[jobCat]": "upload"})
+		let filterData = assets.filter( it => it)
+		let defaultFile = filterData ? filterData[0] : {}
+		let defaultVersion = defaultFile.version
+		let defaultMessage = JSON.parse(defaultFile.message)
+		// 1. 请求文件列表第一条数据的schemas
 		let schemaUrl = "https://api.pharbers.com/schemaexplorer"
+		let reqBody = {
+			"tempfile": defaultMessage.tempfile,
+			"sheet": defaultMessage.sheet,
+			"out_number": 100
+		}
 		let schemaOptions = {
 			method: "POST",
 			mode: "cors",
@@ -49,30 +44,26 @@ export default class MaxSaasImportRoute extends Route {
 				"Content-Type": "application/vnd.api+json",
 				"Accept": "application/vnd.api+json",
 			},
-			body:'{\n    \"tempfile\": \"29538509ce17b6648f6635cb50a86f9d.xlsx\",\n    \"sheet\": \"【辉致项目】2017-2018年6省\",\n    \"start_number\": 1,\n    \"out_number\": 10\n}'
+			body: JSON.stringify(reqBody)
 		}
 		let schemasData = fetch(schemaUrl, schemaOptions).then(res=>res.json())
-		await Promise.all([assets,schemasData, mapperAssets, datasource])
-		let filterData = assets.filter( it => it)
-		//mapper数据
-		let jobLogs = await this.store.query("jobLog", { "filter[provider]": params.provider, "filter[version]": filterData[0] ? filterData[0].version : '', "filter[jobCat]": "mapper"})
-		//excel表格数据
-		let schemas = {}
-		if(filterData[0] && filterData[0].message) {
-			//获取源数据表头
-			let message = JSON.parse(filterData[0].message)
-			let sheetName =  message.sheet
-			let schemasResult = schemasData._result ? schemasData._result[0] : {}
-			schemas = schemasResult.schema
-		}
+		// 2. 请求mapper数据
+		let defaultJobLog = this.store.query("jobLog", { "filter[provider]": params.provider, "filter[version]": defaultVersion, "filter[jobCat]": "mapper"})
+		// 3. 文件列表状态显示
+		let mapperAssets = this.store.query("jobLog", { "filter[provider]": params.provider, "filter[time]": time, "filter[jobCat]": "mapper"})
+		await Promise.all([schemasData, defaultJobLog, mapperAssets])
+		// 4. schemas默认数据处理
+		let schemasResult = schemasData._result ? schemasData._result[0] : {}
+		let schemas = schemasResult ? schemasResult.schema : []
+		console.log("jobLogs",defaultJobLog.filter( it => it))
+		console.log("mapperAssets",mapperAssets.filter( it => it))
         return RSVP.hash({
-			jobLogs: jobLogs.filter( it => it), //mapper弹框数据
+			jobLogs: defaultJobLog.filter( it => it), //mapper弹框数据
             assets: filterData, //文件列表
 			mapperAssets: mapperAssets.filter( it => it), //列表状态
 			schemas: schemas, //源数据表头
 			fileName: filterData[0] ? filterData[0].name : '', //当前文件名称
-			sourceData: JSON.parse(datasource._result.body).slice(0,5), //目标文件数据
-			targetNames: {"headers":["pack_id","mole_name_en","mole_name_ch","prod_desc","prod_name_ch", "corp_name_ch", "mnf_name_ch", "dosage", "spec", "pack", "atc4_code"], "name": "sourceList"}, //目标文件表头
+			targetNames: ["pack_id","mole_name_en","mole_name_ch","prod_desc","prod_name_ch", "corp_name_ch", "mnf_name_ch", "dosage", "spec", "pack", "atc4_code"], //目标文件表头
 			_isVue: true
         })
     }
--- vue-web-components/vue-excel-component/src/components/bp-excel.vue
+++ vue-web-components/vue-excel-component/src/components/bp-excel.vue
@@ -3,7 +3,7 @@
 	<div class="schemas">
 		<div class="schema-item" v-for="(item,index) in schemas" :key="index+'schema'">{{item}}</div>
 	</div>
-	<div ref="viewport" @click="focusHandler" class="viewport">
+	<div ref="viewport" @click="focusHandler" class="viewport" v-if="!onlyHeaders">
 		<canvas ref="canvas" class="canvas"></canvas>
 		<div ref="select" class="row-select"></div>
 		<select ref="hidden" @keydown="keyPressHandler" style="width: 0;height: 0"></select>
@@ -111,14 +111,21 @@ export default {
 		},
 		schemas: {
 			type: Array,
-			default: ['年', '月份', '季度', '省份', '城市', '区县', '医院库类型', '医院级别', '医院名称', '通用名', '药品名', '匹配名', '商品名', '剂型', '规格', '包装', '生产企业', '价格', '数量', '金额', '价格转换比', '最小使用单位数量', '备注']
+			default: () => []
+		},
+		onlyHeaders: {
+			type: Boolean,
+			default: false
 		}
 	},
 	beforeMount() {
 		this.datasource.refreshData(this)
 	},
 	mounted() {
-		this.focusHandler()
+		//是否请求表体数据
+		if(!onlyHeaders) {
+			this.focusHandler()
+		}
 	},
 	methods: {
 		sheetHitSize() {
