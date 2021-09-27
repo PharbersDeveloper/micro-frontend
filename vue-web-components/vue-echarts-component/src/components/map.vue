1cbd5995d2e8c2bd1c3365d433c209805bdab702
wodelu <1058744843@qq.com>
2021-09-27 20:13:49 +0800

    下钻及效果

--- vue-web-components/vue-echarts-component/src/components/map.vue
+++ vue-web-components/vue-echarts-component/src/components/map.vue
@@ -8,70 +8,109 @@
 import * as echarts from 'echarts';
 import 'echarts/map/js/china.js'
 import neimenggu from '../assets/proviceMap/neimenggu.json'
+import hulunbeiershi from '../assets/proviceMap/hulunbeiershi.json'
 export default {
     data() {
         return {
-            //echart 配制option  
-            options: {
-                tooltip: {
-                    triggerOn: "mousemove",   //mousemove、click
-                    padding:8,
-                    borderWidth:1,
-                    borderColor:'#409eff',
-                    backgroundColor:'rgba(255,255,255,0.7)',
-                    textStyle:{
-                        color:'#000000',
-                        fontSize:13
-                    },
-                    formatter: function(e, t, n) {
-                        let data = e.data;
-                        //模拟数据
-                        data.specialImportant = Math.random()*1000 | 0;
-                        data.import = Math.random()*1000 | 0;
-                        data.compare = Math.random()*1000 | 0;
-                        data.common = Math.random()*1000 | 0;
-                        data.specail = Math.random()*1000 | 0;
+            
+        }
+    },
+    methods: {
+        //初始化中国地图
+        initEchartMap() {
+            let that = this
+            let mapDiv = document.getElementById('china_map');
+            let chart = echarts.init(mapDiv);
+            let name = "内蒙古";
 
-                        let context = `
-                        <div>
-                            <p><b style="font-size:15px;">${data.name}</b>产品表现</b></p>
-                            <p class="tooltip_style"><span class="tooltip_left">事件总数</span><span class="tooltip_right">${data.value}</span></p>
-                            <p class="tooltip_style"><span class="tooltip_left">特别重大事件</span><span class="tooltip_right">${data.specialImportant}</span></p>
-                            <p class="tooltip_style"><span class="tooltip_left">重大事件</span><span class="tooltip_right">${data.import}</span></p>
-                            <p class="tooltip_style"><span class="tooltip_left">较大事件</span><span class="tooltip_right">${data.compare}</span></p>
-                            <p class="tooltip_style"><span class="tooltip_left">一般事件</span><span class="tooltip_right">${data.common}</span></p>
-                            <p class="tooltip_style"><span class="tooltip_left">特写事件</span><span class="tooltip_right">${data.specail}</span></p>
-                        </div>
-                        `
-                        return context;
-                    }
-                },
-                visualMap: {
-                    show:true,
-                    left: 26,
-                    bottom: 40,
-                    showLabel:true,
-                    inRange: {
-                        // 由深到浅
-                        color: [
-                            '#B29106',
-                            '#F9D43F',
-                            '#F9D43F',
-                            '#FBE074',
-                            '#F7E065',
-                            '#FCE99D',
-                            '#FBF5BA'
-                        ]
-                    }
-                },
+            chart.resize({
+                animation: {
+                    duration: 1
+                }
+            })
+            // let dataList = [
+            //     {
+            //         name: "南海诸岛",
+            //         value: 100,
+            //         eventTotal:100,
+            //         specialImportant:10,
+            //         import:10,
+            //         compare:10,
+            //         common:40,
+            //         specail:20
+            //     },
+            //     {
+            //         name: "北京",
+            //         value: 1
+            //     },
+            //     {
+            //         name: "内蒙古",
+            //         value: 600
+            //     }
+            // ]
+            let options = {
+                // tooltip: {
+                //     triggerOn: "mousemove",   //mousemove、click
+                //     padding:8,
+                //     borderWidth:1,
+                //     borderColor:'#409eff',
+                //     backgroundColor:'rgba(255,255,255,0.7)',
+                //     textStyle:{
+                //         color:'#000000',
+                //         fontSize:13
+                //     },
+                //     formatter: function(e, t, n) {
+                //         let data = e.data ? e.data : {};
+                //         //模拟数据
+                //         data.specialImportant = Math.random()*1000 | 0;
+                //         data.import = Math.random()*1000 | 0;
+                //         data.compare = Math.random()*1000 | 0;
+                //         data.common = Math.random()*1000 | 0;
+                //         data.specail = Math.random()*1000 | 0;
+
+                //         let context = `
+                //         <div>
+                //             <p><b style="font-size:15px;">${data.name}</b>产品表现</b></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">事件总数</span><span class="tooltip_right">${data.value}</span></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">特别重大事件</span><span class="tooltip_right">${data.specialImportant}</span></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">重大事件</span><span class="tooltip_right">${data.import}</span></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">较大事件</span><span class="tooltip_right">${data.compare}</span></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">一般事件</span><span class="tooltip_right">${data.common}</span></p>
+                //             <p class="tooltip_style"><span class="tooltip_left">特写事件</span><span class="tooltip_right">${data.specail}</span></p>
+                //         </div>
+                //         `
+                //         return context;
+                //     }
+                // },
+                // visualMap: {
+                //     show:true,
+                //     left: 26,
+                //     bottom: 40,
+                //     showLabel:true,
+                //     inRange: {
+                //         // 由深到浅
+                //         color: [
+                //             '#B29106',
+                //             '#F9D43F',
+                //             '#F9D43F',
+                //             '#FBE074',
+                //             '#F7E065',
+                //             '#FCE99D',
+                //             '#FBF5BA'
+                //         ]
+                //     }
+                // },
                 geo: {
                     map: "china",
+                    // roam: true,
+                    // zoom: 1,
+                    layoutCenter: ['50%', '50%'],
+                    layoutSize: '50%',
                     scaleLimit: {
-                        min: 1,
+                        min: 0.5,
                         max: 2
                     },
-                    zoom: 1,
-                    top: 120,
+                    top: 200,
                     label: {
                         normal: {
                             show:true,
@@ -99,184 +138,22 @@ export default {
                 series: [
                     {
                         type: "map",
-                        geoIndex: 0,
-                        data:[]
+                        geoIndex: 0
                     }
                 ]
-            },
-            //echart data
-            dataList: [
-                {
-                    name: "南海诸岛",
-                    value: 100,
-                    eventTotal:100,
-                    specialImportant:10,
-                    import:10,
-                    compare:10,
-                    common:40,
-                    specail:20
-                },
-                {
-                    name: "北京",
-                    value: 540
-                },
-                {
-                    name: "天津",
-                    value: 130
-                },
-                {
-                    name: "上海",
-                    value: 400
-                },
-                {
-                    name: "重庆",
-                    value: 750
-                },
-                {
-                    name: "河北",
-                    value: 130
-                },
-                {
-                    name: "河南",
-                    value: 830
-                },
-                {
-                    name: "云南",
-                    value: 110
-                },
-                {
-                    name: "辽宁",
-                    value: 19
-                },
-                {
-                    name: "黑龙江",
-                    value: 150
-                },
-                {
-                    name: "湖南",
-                    value: 690
-                },
-                {
-                    name: "安徽",
-                    value: 60
-                },
-                {
-                    name: "山东",
-                    value: 39
-                },
-                {
-                    name: "新疆",
-                    value: 4
-                },
-                {
-                    name: "江苏",
-                    value: 31
-                },
-                {
-                    name: "浙江",
-                    value: 104
-                },
-                {
-                    name: "江西",
-                    value: 36
-                },
-                {
-                    name: "湖北",
-                    value: 52
-                },
-                {
-                    name: "广西",
-                    value: 33
-                },
-                {
-                    name: "甘肃",
-                    value: 7
-                },
-                {
-                    name: "山西",
-                    value: 5
-                },
-                {
-                    name: "内蒙古",
-                    value: 778
-                },
-                {
-                    name: "陕西",
-                    value: 22
-                },
-                {
-                    name: "吉林",
-                    value: 4
-                },
-                {
-                    name: "福建",
-                    value: 18
-                },
-                {
-                    name: "贵州",
-                    value: 5
-                },
-                {
-                    name: "广东",
-                    value: 98
-                },
-                {
-                    name: "青海",
-                    value: 1
-                },
-                {
-                    name: "西藏",
-                    value: 0
-                },
-                {
-                    name: "四川",
-                    value: 44
-                },
-                {
-                    name: "宁夏",
-                    value: 4
-                },
-                {
-                    name: "海南",
-                    value: 22
-                },
-                {
-                    name: "台湾",
-                    value: 3
-                },
-                {
-                    name: "香港",
-                    value: 5
-                },
-                {
-                    name: "澳门",
-                    value: 555
-                }
-            ]
-        };
-    },
-    methods: {
-    //初始化中国地图
-        initEchartMap() {
-            let that = this
-            let mapDiv = document.getElementById('china_map');
-            let myChart = echarts.init(mapDiv);
-            myChart.setOption(this.options);
-            //下面的事件需要写在上面的getDownMap()里
-            myChart.on('click', function(params){
-                debugger
-                //如果数组没有代表到了区县，跳出循环
-                // if(that.mapList.indexOf(params.name) < 0){
-                //     return
-                // }
+            }
+            echarts.registerMap(name,neimenggu);
+            chart.setOption(options);
+            chart.on('click', function(params){
                 //根据点击的市，加载不同json
                 let mapJson;
                 switch(params.name){
-                case "内蒙古":
-                    mapJson = neimenggu;
+                case '呼和浩特市':
+                    mapJson = huhehaoteshi;
                     break;
-                case '包头市':
-                    mapJson = baotoushi;
+                case '内蒙古':
+                    options.geo.map = '内蒙古'
+                    mapJson = neimenggu;
                     break;
                 case '乌海市':
                     mapJson = wuhaishi;
@@ -309,18 +186,11 @@ export default {
                     mapJson = alashanmeng;
                     break; 
                 }
-                //下面的name需要为“内蒙古”，而不是具体点击的市
-                echarts.registerMap('内蒙古',mapJson);
-                myChart.setOption(that.options);
+                //下面的name需要为“内蒙古”
+                echarts.registerMap(name,mapJson);
+                chart.setOption(options);
             })
-        },
-        //修改echart配制
-        setEchartOption(){
-            this.options.series[0]['data'] = this.dataList;
         }
-    },
-    created() {
-        this.setEchartOption();
     },
     mounted() {
         this.$nextTick(()=>{
@@ -331,7 +201,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
     #china_map_box {
         height: 100%;
         position: relative;
@@ -339,23 +209,21 @@ export default {
     #china_map_box #china_map{
         height: 100%;
     }
-     #china_map_box .china_map_logo{
+    #china_map_box .china_map_logo{
         position: absolute;
         top: 0;
         left: 0;
         width:45px;
-     }
-</style>
-<style>
-  #china_map .tooltip_style{
-      line-height:1.7;
-      overflow: hidden;
-  }
-  #china_map .tooltip_left{
-      float: left;
-  }
-  #china_map .tooltip_right{
-      float: right;
-  }
+    }
+    #china_map .tooltip_style{
+        line-height:1.7;
+        overflow: hidden;
+    }
+    #china_map .tooltip_left{
+        float: left;
+    }
+    #china_map .tooltip_right{
+        float: right;
+    }
 
 </style>
