0eec144bc68f83163fc4c1a4e20afbe6922a86ed
wodelu <1058744843@qq.com>
2021-10-13 12:00:14 +0800

    修改提示框内容

--- vue-web-components/vue-echarts-component/src/components/heatmap-chart.vue
+++ vue-web-components/vue-echarts-component/src/components/heatmap-chart.vue
@@ -12,10 +12,11 @@ export default {
         return {
             heatmapChart: null,
             timer: null,
-			atc3Arr: [],
-			numberArr: [],
-			dataSalesArr: [],
-			maxSales: 0
+            atc3Arr: [],
+            numberArr: [],
+            dataSalesArr: [],
+            maxSales: 0,
+            result: []
         }
     },
     mounted () {
@@ -48,7 +49,7 @@ export default {
         },
         async queryData() {
             const url = "https://api.pharbers.com/phchproxyquery"
-            const accessToken = this.getCookie("access_token") || "aa724ba2d728d15dba6c7bd80da753c03ed8316857d98f07982a19b62763d78d"
+            const accessToken = this.getCookie("access_token") || "d0fcdb3bf8fe8dd91cdb4ef64815013e1cce9444bb6d44dc981b554cab3c12c4"
             let body = {"query":"select atc3,splitByString('_', arr)[1] as atc5, toFloat64(splitByString('_', arr)[2]) as sales, number from (select atc3,groupArray(atc_sales) AS arr_val from (select atc3, concat(atc, '_', toString(sales)) as atc_sales from (select atc, substring(atc,1,3) as atc3, sum(sales) as sales from phmax.data_wide where atc != 'null' and length(atc)=5 GROUP BY atc ORDER BY atc3 ASC,sales DESC) table1 ) table2 GROUP BY atc3 ) table3　array join arr_val as arr, arrayEnumerate(arr_val) AS number where number <=10","schema":["atc3","atc5", "sales", "number"]}
             let options = {
                 method: "POST",
@@ -60,39 +61,39 @@ export default {
                 body: JSON.stringify(body)
             }
             let result = await fetch(url, options).then(res => res.json())
-			this.atc3Arr = []
-			this.numberArr = []
-			this.dataSalesArr = []
-			this.maxSales = 0
-			let minNum = 100
-			let maxNum = 0
+            this.atc3Arr = []
+            this.numberArr = []
+            this.dataSalesArr = []
+            this.maxSales = 0
+            let minNum = 100
+            let maxNum = 0
             result.forEach((item, index) => {
-				if(Number(item.sales) > this.maxSales) {
-					this.maxSales = Number(item.sales)
-				}
-				//atc3 y轴数据
-				if(this.atc3Arr.indexOf(item.atc3) == -1) {
-					this.atc3Arr.push(item.atc3)
-				}
+                if(Number(item.sales) > this.maxSales) {
+                    this.maxSales = Number(item.sales)
+                }
+                //atc3 y轴数据
+                if(this.atc3Arr.indexOf(item.atc3) == -1) {
+                    this.atc3Arr.push(item.atc3)
+                }
 
-				if(Number(item.number) > maxNum) {
-					maxNum = Number(item.number)
-				}
-				if(Number(item.number) < minNum) {
-					minNum = Number(item.number)
-				}
+                if(Number(item.number) > maxNum) {
+                    maxNum = Number(item.number)
+                }
+                if(Number(item.number) < minNum) {
+                    minNum = Number(item.number)
+                }
 
-				let atc5fitst = item.atc5.slice(0,3)
-				let yNum = this.atc3Arr.indexOf(atc5fitst)
-				let dataArr = []
-				dataArr.push(item.number, yNum, item.sales)
-				this.dataSalesArr.push(dataArr)
+                let atc5fitst = item.atc5.slice(0,3)
+                let yNum = this.atc3Arr.indexOf(atc5fitst)
+                //data数据
+                let dataArr = []
+                dataArr.push(item.number, yNum, item.sales)
+                this.dataSalesArr.push(dataArr)
             })
-			const rangeArray = (start, end) => Array(end - start + 1).fill(0).map((v, i) => i + start)
-			//x轴数据
-			this.numberArr = rangeArray(minNum, maxNum)
-			console.log(this.dataSalesArr)
-            return result
+            const rangeArray = (start, end) => Array(end - start + 1).fill(0).map((v, i) => i + start)
+            //x轴数据
+            this.numberArr = rangeArray(minNum, maxNum)
+            this.result = result
         },
         getCookie(name) {
             let arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
@@ -102,61 +103,69 @@ export default {
                 return null;
         },
         renderheatmapChart () {
-			let option = {
-				tooltip: {
-					position: 'top',
-					trigger: 'item',
-                    // formatter: '{a}<br/>{b}<br/>{c}',
-					formatter: function (params) {
-                        let tar = params;
-                        return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+            let that = this
+            let option = {
+                tooltip: {
+                    position: 'top',
+                    trigger: 'item',
+                    formatter: function (params) {
+                        let atc5Value = ''
+                        let xcoor = params.value[0]
+                        let ycoor = that.atc3Arr[params.value[1]]
+                        let sales = params.value[2]
+                        that.result.forEach(item => {
+                            if(item.atc3 == ycoor && xcoor == Number(item.number)) {
+                                atc5Value = item.atc5
+                            }
+                        })
+                        return atc5Value + '<br/>' + sales
                     },
                     backgroundColor:'rgba(28,36,39,0.8)',
-                    borderColor: 'rgba(28,36,39,1)',
-				},
-				grid: {
-					// height: '50%',
-					// top: '5%'
-				},
-				xAxis: {
-					type: 'category',
-					data: this.numberArr,
-					splitArea: {
-						show: true
-					}
-				},
-				yAxis: {
-					type: 'category',
-					data: this.atc3Arr,
-					splitArea: {
-						show: true
-					}
-				},
-				visualMap: {
-					min: 0,
-					max: this.maxSales,
-					calculable: true,
-					orient: 'horizontal',
-					left: 'center',
-					top: 0
-				},
-				series: [
-					{
-						name: 'heatmap',
-						type: 'heatmap',
-						data: this.dataSalesArr,
-						label: {
-							show: true
-						},
-						emphasis: {
-							itemStyle: {
-								shadowBlur: 10,
-								shadowColor: 'rgba(0, 0, 0, 0.5)'
-							}
-						}
-					}
-				]
-			};
+                    borderColor: 'rgba(28,36,39,1)'
+                },
+                grid: {
+                    // height: '50%',
+                    // top: '5%'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: this.numberArr,
+                    splitArea: {
+                        show: true
+                    }
+                },
+                yAxis: {
+                    type: 'category',
+                    data: this.atc3Arr,
+                    splitArea: {
+                        show: true
+                    }
+                },
+                visualMap: {
+                    min: 0,
+                    max: this.maxSales,
+                    calculable: true,
+                    orient: 'horizontal',
+                    left: 'center',
+                    top: 0
+                },
+                series: [
+                    {
+                        name: 'heatmap',
+                        type: 'heatmap',
+                        data: this.dataSalesArr,
+                        label: {
+                            show: true
+                        },
+                        emphasis: {
+                            itemStyle: {
+                                shadowBlur: 10,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
             // 绘制图表
             this.heatmapChart.setOption(option)
         }
