211d4cdb8d76f9e84db19362e375caf164cd6823
wodelu <1058744843@qq.com>
2021-10-28 16:58:54 +0800

    新建models

---
+++ bpcatelogpage/app/models/partition.js
@@ -0,0 +1,9 @@
+import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
+
+export default class PartitionModel extends Model {
+	@attr('string') smID;
+	@attr('string') source;
+	@attr('string') schema;
+	@attr('number') date;
+	@attr('string') partitions;
+}
