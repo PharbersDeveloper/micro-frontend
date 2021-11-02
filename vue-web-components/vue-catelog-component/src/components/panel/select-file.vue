3a7ceb53031d97dae8710e58b636107e7798a257
wodelu <1058744843@qq.com>
2021-11-02 19:01:55 +0800

    修改样式

--- vue-web-components/vue-catelog-component/src/components/panel/select-file.vue
+++ vue-web-components/vue-catelog-component/src/components/panel/select-file.vue
@@ -18,7 +18,14 @@
                     <div class="next" @click="open" v-if="fileList.length > 0">下一步</div>
                     <input style="display: none;" type="file" name="" ref="file" @change="uploadFiles" accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv,.csv,.xlsx,.xls,.xlsm">
                 </div>
+                <div class="select-area">
+                    <span class="title">Upload to connection</span>
+                    <bp-select-vue src="https://www.pharbers.com/public/icon_drop.svg" :choosedValue="selectParam">
+                        <bp-select-option @click="changeSelect">本集群</bp-select-option>
+                    </bp-select-vue>
+                </div>
             </div>
+            
         </div>
         <next-dialog v-if="show" @closeDialog="close" :fileList="fileList" @uploadFilesEvent="uploadFilesEvent"></next-dialog>
     </div>
@@ -26,15 +33,20 @@
 
 <script>
 import nextDialog from './next-dialog'
+import bpSelectVue from '../../../node_modules/vue-components/src/components/bp-select-vue.vue'
+import bpOptionVue from '../../../node_modules/vue-components/src/components/bp-option-vue.vue'
 export default {
     data() {
         return {
             show: false,
-            fileList: []
+            fileList: [],
+            selectParam: "本集群"
         }
     },
     components: {
-        nextDialog
+        nextDialog,
+        bpSelectVue,
+        bpOptionVue
     },
     props: {
         allData: {
@@ -46,6 +58,9 @@ export default {
         }
     },
     methods: {
+        changeSelect() {
+            
+        },
         open() {
             this.show = true
         },
@@ -84,11 +99,11 @@ export default {
 .upload_dataset_container {
     width: 100vw;
     height: 100vh;
-    border: 2px solid #979797;
+    border: 2px solid #ddd;
     .project_name_header {
         height: 50px;
         width: 100%;
-        border-bottom: 2px solid #979797;
+        border-bottom: 2px solid #ddd;
         .project_name {
             margin-left: 30px;
             line-height: 50px;
@@ -103,6 +118,7 @@ export default {
     }
     .upload_file_area {
         width: 100%;
+        padding: 20px;
         height: calc(100vh - 110px);
         background: #f7f7f7;
         padding-top: 50px;
@@ -133,13 +149,34 @@ export default {
                 background: none;
             }
         }
+        .select-area {
+            width: 100%;
+            background: #fff;
+            margin-top: 20px;
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border: 1px solid #979797;
+            .title {
+                width: 190px;
+            }
+            /deep/.bp-select {
+                height: 26px;
+            }
+            /deep/.bp-select-title{
+                background: #fff !important;
+                height: 26px;
+                border: 1px solid #cccccc;
+            }
+        }
         .upload_area {
             position: relative;
-            width: 1400px;
-            height: 400px;
+            // width: 1400px;
+            width: 100%;
+            height: 184px;
             border: 1px solid #979797;
             margin: 0 auto;
-            background: #d8d8d8;
+            background: #FFF;
             .select {
                 position: absolute;
                 top: 0;
