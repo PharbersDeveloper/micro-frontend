0b274c3c7607f7d7452487c7572b66e84f4da7e3
AlfredYang1986 <yangyuanpig@gmail.com>
2021-11-04 15:24:17 +0800

    数据导入的拼接

--- bpcatelogpage/app/components/excel-handler.js
+++ bpcatelogpage/app/components/excel-handler.js
@@ -8,14 +8,20 @@ export default class ExcelHandlerComponent extends Component {
     @service cookies
     @service ajax
 
-
 	@action
     async listener(e) {
         switch(e.detail[0].args.callback) {
             case "linkToPage":
-                let param = e.detail[0].args.param
+                const ltp = e.detail[0].args.param
+                console.log(ltp)
                 break
-            default: 
+            case "createDataSetIndex":
+                debugger
+                const param = e.detail[0].args.param
+                console.log(param)
+                this.createDataSetIndex(param)
+                break
+            default:
                 console.log("submit event to parent")
         }
     }
@@ -35,5 +41,38 @@ export default class ExcelHandlerComponent extends Component {
     get calAllData() {
         this.args.model._isVue = true
         return this.args.model
+    }
+
+    createDataSetIndex(param) {
+        const that = this
+        const push_type = "put_item"
+        const project_files_body = {
+            "table": "action",
+            "item": {
+                "projectId": "Max", //TODO: 用projectId 替换
+                "code": 0,
+                "comments": "project file to Data set",
+                "jobCat": "project_file_to_DS",
+                "jobDesc": "creating",
+                "message": param,
+                "date": Date.now(),
+                "owner": this.cookies.read('access_token'), // TODO: 用用户id替换
+                "showName": "alfred"
+            }
+        }
+
+        debugger
+        const url = "https://apiv2.pharbers.com/phdydatasource/"
+        const headers = {
+            "Authorization": this.cookies.read( "access_token" ),
+            "Content-Type": "application/vnd.api+json",
+            "Accept": "application/vnd.api+json",
+        }
+        const options = {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(project_files_body)
+        }
+        return fetch(url+push_type, options).then(res => res.json())
     }
 }
