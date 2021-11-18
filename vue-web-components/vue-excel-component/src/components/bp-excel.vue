a8ed656c0edc24c8cdf2447e64404ccbb824c3cb
wodelu <1058744843@qq.com>
2021-11-18 19:40:24 +0800

    解决报错

--- vue-web-components/vue-excel-component/src/components/bp-excel.vue
+++ vue-web-components/vue-excel-component/src/components/bp-excel.vue
@@ -2,9 +2,10 @@
     <div ref="viewport" @click="focusHandler" class="viewport" :style="{height: viewHeight, width: '100%'}" @scroll="scrollGet($event)">
 	    <div class="excel-container">
 		    <div class="schemas" style="width: 100%" ref="schemas">
-			<div class="view" >
-				<header-item v-for="(item, index) in datasource.cols" :title="item" :itemWidth="sizePolicy.cell_hit_width" :key="index"/>
-			</div>
+                <div class="view" >
+                    <header-item v-for="(item, index) in datasource.cols" :title="item" :itemWidth="sizePolicy.cell_hit_width" :key="index"/>
+                </div>
+            </div>
 		</div>
 		<div ref="viewport" @click="focusHandler" class="viewport" :style="{height: viewHeight}" @scroll="scrollGet($event)">
 			<div class="body" :style="{height: page_size * sizePolicy.cell_hit_height +'px'}">
@@ -22,16 +23,16 @@ import PhDefaultPalettePolicy from './model/palettepolicy'
 import PhDefaultFontPolicy from './model/fontpolicy'
 import PhDefaultRenderPolicy from './model/renderpolicy'
 export default {
-    data() {
-        return {
-            anchor: {x: 0, y: 0},
+	data() {
+		return {
+			anchor: {x: 0, y: 0},
 
-            // all states
-            needRefresh: 0,
-            dataRefresh: 0,
-            dataAppend: 0,
-            cur_row: 0,
-            cur_page: 0,
+			// all states
+			needRefresh: 0,
+			dataRefresh: 0,
+			dataAppend: 0,
+			cur_row: 0,
+			cur_page: 0,
 
 			renderPolicy: null
 		}
@@ -147,43 +148,43 @@ export default {
 				this.cur_page++
 				const that = this
 
-                if (this.cur_page > this.datasource.data.length / this.page_size - 1) {
-                    this.dataAppend++
-                } else {
-                    that.cur_page = that.cur_page > that.datasource.data.length / that.page_size - 1 ?
-                        that.datasource.data.length / that.page_size - 1 : that.cur_page
-                    that.needRefresh++
-                }
-                break
-            }
-            case "Space": {
-                let cur_page_row = this.cur_page * this.page_size + this.cur_row
-                let cur_data = this.datasource.data[cur_page_row]
-                const event = new Event("event")
-                event.args = {
-                    callback: "modelData",
-                    element: this,
-                    param: {
-                        data: cur_data
-                    }
-                }
-                this.$emit('showModel', event)
-                break
-            }}
-        }
-    },
-    watch: {
-        needRefresh(n, o) {
-            this.renderPolicy.render(this.cur_row, this.cur_page)
-        },
-        dataRefresh(n, o) {
-            this.datasource.data = []
-            this.datasource.refreshData(this)
-        },
-        dataAppend(n, o) {
-            this.datasource.appendData(this)
-        }
-    }
+				if (this.cur_page > this.datasource.data.length / this.page_size - 1) {
+					this.dataAppend++
+				} else {
+					that.cur_page = that.cur_page > that.datasource.data.length / that.page_size - 1 ?
+						that.datasource.data.length / that.page_size - 1 : that.cur_page
+					that.needRefresh++
+				}
+				break
+			}
+			case "Space": {
+				let cur_page_row = this.cur_page * this.page_size + this.cur_row
+				let cur_data = this.datasource.data[cur_page_row]
+				const event = new Event("event")
+				event.args = {
+					callback: "modelData",
+					element: this,
+					param: {
+						data: cur_data
+					}
+				}
+				this.$emit('showModel', event)
+				break
+			}}
+		}
+	},
+	watch: {
+		needRefresh(n, o) {
+			this.renderPolicy.render(this.cur_row, this.cur_page)
+		},
+		dataRefresh(n, o) {
+			this.datasource.data = []
+			this.datasource.refreshData(this)
+		},
+		dataAppend(n, o) {
+			this.datasource.appendData(this)
+		}
+	}
 };
 </script>
 <style lang="scss">
