b872608cbec236443e793d4c76f4ebd02a8f47eb
AlfredYang1986 <yangyuanpig@gmail.com>
2021-11-27 18:49:45 +0800

    页面布局完了 就是抽

--- vue-web-components/vue-codeditor-component/src/App.vue
+++ vue-web-components/vue-codeditor-component/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import phCodeditor from './components/ph-codeditor-page.vue'
+import phCodeditor from './components/ph-codeditor-page'
 
 export default {
     name: 'App',
--- vue-web-components/vue-codeditor-component/src/components/ph-codeditor-page.vue
+++ vue-web-components/vue-codeditor-component/src/components/ph-codeditor-page.vue
@@ -1,166 +1,137 @@
 <template>
-<div class="ace-container" :style="{ height: viewHeight }">
-    <!-- 官方文档中使用 id，这里禁止使用，在后期打包后容易出现问题，使用 ref 或者 DOM 就行 -->
-    <div class="ace-editor" ref="ace" :style="{height:viewHeight}"></div>
-</div>
+    <div class="codeditor-container">
+        <div class="code-io-list">
+            <div class="code-block-list">
+                <span class="title">inputs</span>
+                <div class="ds-lst">
+                    <span class="ds-item" v-for="(item, index) in datasource.inputs" :key="index">{{item.name}}</span>
+                </div>
+            </div>
+            <div class="code-block-list">
+                <span class="title">outputs</span>
+                <div class="ds-lst">
+                    <span class="ds-item" v-for="(item, index) in datasource.outputs" :key="index">{{item.name}}</span>
+                </div>
+            </div>
+        </div>
 
+        <div class="coding-pane">
+            <div class="coding-title">
+                <button class="button">按钮2</button>
+                <button class="button">保存</button>
+            </div>
+            <div class="coding">
+                <ph-codeditor viewHeight="600px" />
+            </div>
+            <div class="coding-footer">
+                <button class="button">Validate</button>
+                <button class="button">Run</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// 引入全局实例
-import ace from 'ace-builds'
-// 主题风格，引入主题后还需要在 options 中指定主题才会生效
-import 'ace-builds/src-min-noconflict/theme-monokai'
-import 'ace-builds/src-min-noconflict/theme-dracula'
-// 支持代码格式， 需要引入具体的语法高亮库才会有对应的语法高亮效果
-import 'ace-builds/src-min-noconflict/mode-javascript'
-import 'ace-builds/src-min-noconflict/mode-json'
-import 'ace-builds/src-min-noconflict/mode-css'
-import 'ace-builds/src-min-noconflict/ext-language_tools'
-import jsWorkerUrl from 'file-loader!ace-builds/src-noconflict/worker-javascript'
-import jsonWorkerUrl from 'file-loader!ace-builds/src-noconflict/worker-json'
-import cssWorkerUrl from 'file-loader!ace-builds/src-noconflict/worker-css'
-ace.config.setModuleUrl('ace/mode/javascript_worker', jsWorkerUrl)
-ace.config.setModuleUrl('ace/mode/json_worker', jsonWorkerUrl)
-ace.config.setModuleUrl('ace/mode/css_worker', cssWorkerUrl)
-ace.config.setModuleUrl(
-    'ace/snippets/javascript',
-    require('file-loader!ace-builds/src-noconflict/snippets/javascript.js')
-)
-ace.config.setModuleUrl('ace/snippets/css', require('file-loader!ace-builds/src-noconflict/snippets/css.js'))
-
-// import { cloneDeep } from 'utils/tools'
-
+import PhCodeditorDatasource from "./model/datasource"
+import phCodeditor from "./ph-codeditor"
 export default {
-    name: 'ph-codeditor',
-    model: {
-        event: 'change'
+    name: 'codeditor-page',
+    components: {
+        phCodeditor
     },
     props: {
-        viewHeight: {
+        projectId: {
             type: String,
-            default: '600px'
+            default: "max"
         },
-        // 编辑器内容
-        value: {
+        jobId: {
             type: String,
-            default: '12345'
+            default: "1qaz4rfv"
         },
-        // 默认语言
-        language: {
+        flowVersion: {
             type: String,
-            default: 'javascript'
+            default: "developer"
         },
-        // 主题，对应主题库 JS 需要提前引入
-        theme: {
+        jobName: {
             type: String,
-            default: 'monokai'
+            default: "1qaz4rfv_max_test_dag_developer_test_job_a"
         },
-        // 是否只读
-        readonly: {
-            type: Boolean,
-            default: false
-        },
-        // 最大行数
-        maxLines: {
-            type: Number,
-            default: 100
-        },
-        // 是否显示全屏按钮
-        withFullscreenBtn: {
-            type: Boolean,
-            default: false
-        },
-        withFooterBtns: {
-            type: Boolean,
-            default: false
+        datasource: {
+            type: Object,
+            default: function() {
+                return new PhCodeditorDatasource('1',
+                    this.projectId, this.jobId, this.flowVersion, this.jobName)
+            }
         }
     },
     data() {
         return {
-            editor: null,
-            isVisible: false,
-            dialogValue: ''
+
         }
     },
     mounted() {
-        // 初始化
-        this.initEditor()
+        this.datasource.refreshData(this)
     },
     watch: {
-        value(val) {
-            if (this.editor.getValue() !== val) {
-                this.editor.setValue(val)
-                this.editor.clearSelection()
-            }
-        }
+
     },
     methods: {
-        // 初始化
-        initEditor() {
-            // 创建实例
-            this.editor = ace.edit(this.$refs.ace, {
-                mode: `ace/mode/${this.language}`,
-                theme: `ace/theme/${this.theme}`,
-                fontSize: 12,
-                tabSize: 4,
-                value: this.value,
-                selectionStyle: 'text',
-                maxLines: this.maxLines,
-                readOnly: this.readonly
-            })
-            // 设置属性等，具体需要可根据官方参数自行设置
-            this.editor.setOptions({
-                enableBasicAutocompletion: true,
-                enableSnippets: true,
-                enableLiveAutocompletion: true,
-                wrap: true,
-                setShowPrintMargin: false
-            })
-            // 设置值改变监听
-            this.editor.on('change', () => {
-                this.$emit('change', this.editor.getValue())
-            })
-        },
-        // 实例方法，高亮某一行
-        gotoLine(lineNumber) {
-            this.editor.gotoLine(lineNumber)
-        },
-        // 全屏编辑
-        fullscreen() {
-            // this.dialogValue = cloneDeep(this.editor.getValue())
-            // this.isVisible = true
-        },
-        closeEditCode() {
-            this.editor.setValue(this.dialogValue)
-            this.editor.clearSelection()
-        },
-        // resize编辑器
-        resize() {
-            this.editor.resize(true)
-        },
-        destroy() {
-            if (this.editor) {
-                this.editor.destroy()
-                this.editor = null
-            }
-        }
-    },
-    beforeDestroy() {
-        this.destroy()
+
     }
 }
 </script>
 
 <style lang='scss' scoped>
-.ace-container {
-    position: relative;
+.codeditor-container {
+    display: flex;
+    flex-direction: row;
 
-    .ace-editor {
-        position: absolute;
-        inset: 7px 0px 0px;
-        height: 100% !important;
+    .code-io-list {
+        display: flex;
+        flex-direction: column;
+        min-width: 180px;
+        border: red 1px solid;
+        text-align: left;
+
+        .code-block-list {
+            display: flex;
+            flex-direction: column;
+
+            .title {
+                font-size: 14px;
+                border-top: red 1px solid;
+            }
+
+            .ds-lst {
+                display: flex;
+                flex-direction: column;
+
+                .ds-item {
+                    border-top: green 1px solid;
+                }
+            }
+
+        }
     }
 
+    .coding-pane {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        .coding-title {
+            display: flex;
+            flex-direction: row-reverse;
+        }
+
+        .coding {
+            padding-bottom: 7px;
+        }
+
+        .coding-footer {
+            display: flex;
+            flex-direction: row;
+        }
+    }
 }
 </style>
