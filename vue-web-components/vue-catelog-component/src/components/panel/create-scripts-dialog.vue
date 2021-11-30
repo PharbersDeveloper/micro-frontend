ae25c09be9ff5e26ecc1ba66cdced84049cc061b
wodelu <1058744843@qq.com>
2021-11-30 13:14:19 +0800

    临时提交

--- vue-web-components/vue-catelog-component/src/components/panel/create-scripts-dialog.vue
+++ vue-web-components/vue-catelog-component/src/components/panel/create-scripts-dialog.vue
@@ -103,30 +103,24 @@
 </template>
 
 <script>
-import ElButton from 'element-ui/packages/button/index'
-import ElInput from 'element-ui/packages/input/index'
+import ElButton from "element-ui/packages/button/index"
+import ElInput from "element-ui/packages/input/index"
 
 export default {
     data() {
         return{
-            label_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/tag.svg",
-            search_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/search.png",
             add_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/components.pharbers.com/add.svg",
-            enter_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/components.pharbers.com/enter.svg",
             search_row: "https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/%E6%90%9C%E7%B4%A2.svg",
-            searchValue: '',
-            tagsArrayShow: [], //展示的tag数组
-            selectedTags: [], //选中的tag数组
-            newTagsArray: [], //新增的tag数组
             datasetListShow: false,
             datasetOutputListShow: false, //显示选中的output
-            searchDataset: '',
-            addDatasetList: [], //已经选中的dataset list
-            remainDatasetList: [], //剩余未选中的dataset list
+            searchDataset: "",
+            addDatasetList: [], //已经选中的输入数据
+            remainDatasetList: [], //剩余未选中的输入数据
+            remainDatasetListOutputs: [],  //剩余未选中的输出数据
             sel_dataset_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/%E7%BB%93%E6%9E%9CDS.svg",
             del_icon: "https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/%E5%88%A0%E9%99%A4+(1).svg",
             dsName: {
-                name: ''
+                name: ""
             },
             oldDatasetList: [], //现有数据集
             showOldDataset: true
@@ -141,65 +135,53 @@ export default {
     },
     computed: {},
     mounted() {
-        this.remainDatasetList = JSON.parse(JSON.stringify(this.datasets))
-        this.remainDatasetListOutputs = JSON.parse(JSON.stringify(this.datasets))
+        //输入输出数据的dataset列表
+        let that = this
+        this.remainDatasetList = []
+        this.remainDatasetListOutputs = []
+        this.datasets.forEach((item, index) => {
+            that.remainDatasetList.push({
+                name: item.name,
+                id: item.id
+            })
+            that.remainDatasetListOutputs.push({
+                name: item.name,
+                id: item.id
+                // schema: item.schema,
+                // version: item.version
+            })
+        })
     },
-    watch: {
-        searchValue: function() {
-            let that = this
-            if(this.searchValue.trim() == '') {
-                if(this.tagsArrayShow.length < this.tagsArray.length) {
-                    this.tagsArrayShow = this.tagsArray
-                }
-            } else {
-                this.tagsArrayShow = []
-                let allArray = this.tagsArray.concat(this.newTagsArray)
-                allArray.forEach(item => {
-                    let idx = item.indexOf(that.searchValue)
-                    if(idx == 0) {
-                        this.tagsArrayShow.push(item)
-                    }
-                })
-            }
-        },
-        datasetList: function(n, o) {
-            this.datasetList = JSON.parse(JSON.stringify(n))
-        }
-    },
+    watch: {},
     methods: {
         save() {
+            debugger
+            /**
+             * 1. 先判断output是新增还是选择原有数据
+             * 2. 若新增，生成一个id（类似f9fab7a26dc2e5ff48bd6e13914bbe79.xlsx），
+             *    创建一个dataset，否则直接3
+             * 3. 调用赵的接口，往action里面插数据
+            */
             const event = new Event("event")
             event.args = {
-                callback: "addTags",
+                callback: "createScripts",
                 element: this,
                 param: {
-                    name: "addTags",
-                    selectedTags: this.selectedTags
+                    name: "createScripts",
+                    jobName: "compute_" + this.dsName.name,
+                    jobId: "",
+                    targetJobId: "",
+                    inputs: this.addDatasetList,
+                    outputs: [],
+                    jobVersion: this.dsName.name + "_version1",
+                    runTime: "python3",
+                    labels: []
                 }
             }
-            this.$emit('addTagsEvent', event)
+            this.$emit("createScripts", event)
         },
         close() {
-            this.tagsArrayShow = []
-            this.tagsArrayShow = this.tagsArray
-            this.$emit('closeCreateDialog');
-        },
-        submit() {
-            if(this.searchValue.trim() != '' && this.tagsArrayShow.indexOf(this.searchValue) == -1) {
-                this.tagsArrayShow = this.tagsArray.filter(it => it != '')
-                this.selectedTags.push(this.searchValue) //默认选中新增tag
-                this.newTagsArray.push(this.searchValue) //关闭弹框前新增的tag array
-                this.tagsArrayShow = this.tagsArrayShow.concat(this.newTagsArray) //将新增tag添加到tagShow数组
-                this.searchValue = ''
-            }
-        },
-        checkedOneTag(tag) {
-            let idIndex = this.selectedTags.indexOf(tag)
-            if(idIndex >= 0) {
-                this.selectedTags.splice(idIndex, 1)
-            } else {
-                this.selectedTags.push(tag)
-            }
+            this.$emit("closeCreateDialog")
         },
         on_clickAddInput() {
             //增加input
@@ -213,7 +195,7 @@ export default {
         },
         on_clickChangeOutput() {
             this.datasetOutputListShow = false
-            this.dsName.name = ''
+            this.dsName.name = ""
         },
         addDataset(data) {
             this.datasetListShow = false
@@ -228,7 +210,7 @@ export default {
         },
         satasetTab(data) {
             //切换tab
-            if(data == 'old') {
+            if(data == "old") {
                 this.showOldDataset = false
             } else {
                 this.showOldDataset = true
@@ -508,60 +490,6 @@ export default {
         width: 280px;
         border-right: 1px solid #ccc;
     }
-    .tags_name {
-        color: #111;
-        font-size: 12px;
-        font-weight: 600;
-        margin-left: 27px;
-        margin-top: 10px;
-    }
-    .tags_list {
-        height: 350px;
-        overflow: auto;
-    }
-    .tags {
-        display: flex;
-        margin-top: 10px;
-        width: 100%;
-        cursor: pointer;
-        .checkout {
-            margin-top: 5px;
-        }
-        .round {
-            display: inline-block;
-            min-width: 14px;
-            width: 14px;
-            height: 14px;
-            border-radius: 7px;
-            background: #7163C5;
-            margin-top: 5px;
-            margin-left: 15px;
-        }
-        .create_tags {
-            font-size: 14px;
-            margin-left: 15px;
-            margin-top: 2px;
-            max-width: 450px;
-            overflow: hidden;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            -webkit-line-clamp: 3;
-            -webkit-box-orient: vertical;
-        }
-        .green {
-            background: green;
-        }
-        .blue {
-            background: #28aadd;
-        }
-    }
-}
-.text_input {
-    height: 24px;
-    width: 520px;
-    padding-left: 30px;
-    outline: none;
-    border: 1px solid #dddddd;
 }
 .btn {
     position: absolute;
@@ -574,66 +502,14 @@ export default {
         cursor: pointer;
     }
 }
-.useing {
-    color: #111;
-    font-size: 12px;
-    font-weight: 600;
-    margin-left: 27px;
-    margin-top: 10px;
-    position: absolute;
-    right: 80px;
-    top: 105px;
-}
-.num_tags {
-    position: absolute;
-    right: 100px;
-}
 .cancel {
     margin-right: 20px;
-    // background-color:#DB4D71;
-    color: #7163C5;
+    color: #409EFF;
+    border-radius: 4px;
 }
 .save {
-    background-color: #7163C5;
+    background-color: #409EFF;
     color: #fff;
-}
-.search {
-    height: 20px;
-    width: 20px;
-    position: absolute;
-    left: 45px;
-    top: 82px;
-}
-.create {
-    display: flex;
-    position: relative;
-    width: 520px;
-    height: 30px;
-    line-height: 30px;
-    padding-left: 30px;
-    font-size: 12px;
-    margin-top: 10px;
-    .add {
-        position: absolute;
-        top: 7px;
-        left: 5px;
-        // margin-top: 5px;
-        width: 15px;
-        height: 15px;
-
-    }
-    .enter {
-        position: absolute;
-        top: 7px;
-        right: 10px;
-        // margin-top: 5px;
-        width: 15px;
-        height: 15px;
-    }
-    // background-color: pink;
-}
-.create:hover {
-    background-color: #e8e8e8;
-    cursor: pointer;
+    border-radius: 4px;
 }
 </style>
