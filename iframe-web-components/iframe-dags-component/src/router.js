85e931a80a527099b193aebf23ea29ab0aa3df45
wodelu <1058744843@qq.com>
2021-12-23 18:07:51 +0800

    合并notive service的测试文件

--- iframe-web-components/iframe-dags-component/src/router.js
+++ iframe-web-components/iframe-dags-component/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import graph from './components/graph-dag-chart.vue'
+import test from './components/test.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
             path: '/graph',
             name: 'graph',
             component: graph
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: test
         }
     ]
 })
