74e6b276714859d1ec379cdd9cd2a272f1610c71
wodelu <1058744843@qq.com>
2022-01-18 11:37:26 +0800

    官网页面

--- client-helper/offweb-model-helper/src/index.js
+++ client-helper/offweb-model-helper/src/index.js
@@ -2,10 +2,16 @@ import { pharbersHomeRouteModel } from "./pharbersHomeRouteModel"
 import { pharbersHomeEventHandler } from "./pharbersHomeEventHandler"
 import { pharbersMaxRouteModel } from "./pharbersMaxRouteModel"
 import { pharbersRwRouteModel } from "./pharbersRwRouteModel"
+import { pharbersConsultingRouteModel } from "./pharbersConsultingRouteModel"
+import { pharbersActivityListRouteModel } from "./pharbersActivityListRouteModel"
+import { pharbersAboutUsRouteModel } from "./pharbersAboutUsRouteModel"
 
 export {
 	pharbersHomeRouteModel,
 	pharbersHomeEventHandler,
 	pharbersMaxRouteModel,
-	pharbersRwRouteModel
+	pharbersRwRouteModel,
+	pharbersConsultingRouteModel,
+	pharbersActivityListRouteModel,
+	pharbersAboutUsRouteModel
 }
