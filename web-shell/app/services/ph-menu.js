a09adac187447315d5b0347716cae610aeefbaba
AlfredYang1986 <yangyuanpig@gmail.com>
2022-02-09 13:25:10 +0800

    开始处理menu

---
+++ web-shell/app/services/ph-menu.js
@@ -0,0 +1,15 @@
+import Service from "@ember/service"
+
+export default class PhMenuService extends Service {
+	queryLayoutByClient() {
+
+	}
+
+	queryClientPages() {
+
+	}
+
+	queryCurrentPage() {
+
+	}
+}
