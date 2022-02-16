a7cd36252a0c815edd394a5f35073617c5a32c38
wodelu <1058744843@qq.com>
2022-02-16 11:14:12 +0800

    general连接deploy

--- client-helper/platform-model-helper/src/eventHandler/phcatelogProjectListEventHandler.js
+++ client-helper/platform-model-helper/src/eventHandler/phcatelogProjectListEventHandler.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 export async function phcatelogProjectListEventHandler(e, route) {
+	const param = e.detail[0].args.param
 	switch (e.detail[0].args.callback) {
 		case "linkToPage":
-			window.open("https://deploy.pharbers.com/projects/JfSmQBYUpyb4jsei")
-
+			window.open(`https://deploy.pharbers.com/projects/${param.pid}`)
 			break
 		default:
 			console.log("submit event to parent")
