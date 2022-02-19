614a01e62a17d8f72450bd35e42813e6a76cfaa6
Alex <891366402@qq.com>
2022-02-19 20:47:55 +0800

    代码逻辑优化，加入缺少的unregister逻辑，前端直接更新无需改变操作

--- web-shell/app/lib/iot/PhHttpMqtt.js
+++ web-shell/app/lib/iot/PhHttpMqtt.js
@@ -5,14 +5,12 @@
 function PhHttpMQTT(config, callBack, timeoutQueue) {
     let intervalId = null;
     const topic = config.topic || "pharbers";
-    const client_id = config.client_id;
     const endpoint = config.endpoint;
-    const qos = 1 // LeastOnce
 
     const __subscribe = () => {
         intervalId = setInterval(() => {
-            // Query http mqtt interface 获取的数据交给callBack
-        }, 1000 * 2) // 2秒 后续可编程参数，先实现
+            // Call Query http mqtt interface 获取的数据交给callBack
+        }, 1000 * 5) // 5秒 后续可编程参数，先实现
     }
 
     const connect = async () => {
--- web-shell/app/services/execution-status.js
+++ web-shell/app/services/execution-status.js
@@ -1,13 +1,10 @@
 import Service from '@ember/service';
 import ENV from "web-shell/config/environment";
-import { inject as service } from "@ember/service"
-import fetch from "fetch"
 import Iot from '../lib/iot/PhIot';
 
 
 export default class ExecutionStatusService extends Service {
 
-    @service cookies
 
     endpoint = ENV.APP.AWS_IOT_ENDPOINT
     aws_region = ENV.APP.AWS_REGION
@@ -23,63 +20,28 @@ export default class ExecutionStatusService extends Service {
 
     functions = {
         "iot": async (item) => {
-            const topic = `${item.projectId}/${item.ownerId}`
-            const iotId = this.iotInstance.find((iot) => iot.id == item.id)
-            if (!iotId) {
-                const iotTopic = this.iotInstance.find((iot) => iot.topic == topic)
-                if (iotTopic) {
-                    this.destroyQueue.push(iotTopic.id)
-                }
-                const iotConfig = {
-                    id: item.id,
-                    type: this.mqtt_type,
-                    topic: topic,
-                    client_id: item.ownerId,
-                    endpoint: this.endpoint,
-                    aws_region: this.aws_region,
-                    aws_access_id: this.aws_access_id,
-                    aws_secret_key: this.aws_secret_key,
-                    timeout: item.timeout || this.timeout,
-                    parameter: item
-                }
-                const iot = Iot.PhIot.newInstance(iotConfig, item.callBack, this.destroyQueue)
-                this.iotInstance.push({
-                    id: item.id,
-                    topic: topic,
-                    ins: iot
-                })
-                await iot.connect()
+            const iot = this.iotInstance.find((iot) => iot.topic == topic)
+            if (iot) {
+                this.destroyQueue.push(iot.topic)
             }
+            const iotConfig = {
+                type: this.mqtt_type,
+                topic: item.topic,
+                client_id: item.ownerId,
+                endpoint: this.endpoint,
+                aws_region: this.aws_region,
+                aws_access_id: this.aws_access_id,
+                aws_secret_key: this.aws_secret_key,
+                timeout: item.timeout || this.timeout,
+                parameter: item
+            }
+            const iot = Iot.PhIot.newInstance(iotConfig, item.callBack, this.destroyQueue)
+            this.iotInstance.push({
+                topic: item.topic,
+                ins: iot
+            })
+            await iot.connect()
         },
-        // "other": (item) => {
-        //     const parameter = Object.assign({}, item)
-        //     delete parameter.callBack
-        //     item.callBack(parameter, this.destroyQueue)
-        // },
-        // "executionStatus": async (item) => {
-        //     const url = "https://apiv2.pharbers.com/phdydatasource/query"
-        //     const headers = {
-        //         Authorization: that.cookies.read("access_token"),
-        //         "Content-Type": "application/vnd.api+json",
-        //         Accept: "application/vnd.api+json",
-        //     }
-        //     const body = {
-        //         table: item.tableName,
-        //         conditions: {
-        //             "id": ["=", `${item.projectId}${item.ownerId}`],
-        //             "date": ["<", `${new Date().getTime()}`]
-        //         }
-        //     }
-        //     const response = await fetch(url, {
-        //         method: "POST",
-		// 			headers: headers,
-		// 			body: JSON.stringify(body)
-        //     })
-        //     if (response) {
-        //         this.__destroyQueue.push(item.id)
-        //         item.callBack(response)
-        //     }
-        // }
 
     }
 
@@ -87,34 +49,36 @@ export default class ExecutionStatusService extends Service {
         {   
             type,
             id,
-            tableName,
             projectId,
             ownerId,
             callBack,
             timeout = this.timeout
         } = {}) {
-        if (!this.action.find(item => item.id == id)) {
-            this.action.push({
+        const topic = `${projectId}/${ownerId}/${id}`
+        if (!this.action.find(item => item.topic || "" == topic)) {
+            const item = {
                 type: type,
+                topic: topic,
                 id: id,
-                tableName: tableName,
                 projectId: projectId,
                 ownerId: ownerId,
                 timeout: timeout,
                 callBack: callBack,
-            })
+            }
+            this.action.push(item)
+            this.functions[item.type](item)
         } else {
-            console.warn("id 重复")
+            console.warn("重复定义操作")
         }
         if (!this.startOb) { 
             this.__observer()
         }
     }
 
-    __unregister(id) {
-        this.action = this.action.filter(item => item.id != id)
-        const iot = this.iotInstance.find((item) => item.id == id)
-        this.iotInstance = this.iotInstance.filter(item => item.id != id)
+    __unregister(topic) {
+        this.action = this.action.filter(item => item.topic != topic)
+        const iot = this.iotInstance.find((item) => item.topic == topic)
+        this.iotInstance = this.iotInstance.filter(item => item.topic != topic)
         if (iot) { 
             iot.ins.disconnect()
         }
@@ -122,24 +86,24 @@ export default class ExecutionStatusService extends Service {
 
     __destroyQueue() {
         setInterval(() => {
-            const id = this.destroyQueue.shift()
-            if (id) {
-                this.__unregister(id)
+            const topic = this.destroyQueue.shift()
+            if (topic) {
+                this.__unregister(topic)
             }
-        }, 1000)
+        }, 1000 * 1)
     }
 
     __observer() {
-        this.__destroyQueue();
         this.startOb = true;
-        setInterval(() => {
-            if (this.action.length > 0) {
-                this.action.forEach(async (item, index) => {
-                    this.functions[item.type](item)
-                })
-            } else {
-                console.log("notice observer")
-            }
-        }, 500 * 1)
+        this.__destroyQueue();
+        // setInterval(() => {
+        //     if (this.action.length > 0) {
+        //         this.action.forEach(async (item, index) => {
+        //             this.functions[item.type](item)
+        //         })
+        //     } else {
+        //         console.log("notice observer")
+        //     }
+        // }, 500 * 1)
     }
 }
