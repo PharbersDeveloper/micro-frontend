aafbcdcfa51e1c820827314593cb4d7f10fb6e5e
AlfredYang1986 <yangyuanpig@gmail.com>
2022-02-23 17:29:20 +0800

    d3 的图表demo

---
+++ vue-web-components/vue-histogram-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <histogram msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+import histogram from './components/histograme.vue'
+
+export default {
+    name: 'App',
+    components: {
+        histogram
+    }
+}
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
