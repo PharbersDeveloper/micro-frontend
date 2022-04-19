4909976b795bf1ff055a048ed7d49ed8ed69fa10
AlfredYang1986 <yangyuanpig@gmail.com>
2022-04-19 18:50:15 +0800

    缺datasource测试

--- vue-web-components/vue-analyze-component/src/components/prepareCal/steps/filter/commands/FilterOnValue.js
+++ vue-web-components/vue-analyze-component/src/components/prepareCal/steps/filter/commands/FilterOnValue.js
@@ -19,7 +19,7 @@ export default class PhFilterOnValueCmd {
     }
 
     validations() {
-
+        return true
     }
 
     revert2Defs() {
--- vue-web-components/vue-analyze-component/src/components/prepareCal/steps/filter/filter-steps.js
+++ vue-web-components/vue-analyze-component/src/components/prepareCal/steps/filter/filter-steps.js
@@ -1,5 +1,6 @@
 
 import { staticFilePath, hostName } from "../../../../config/envConfig"
+import PhFilterOnValueCmd from "./commands/FilterOnValue"
 
 /**
  * 这个就是我所说的Command
@@ -8,9 +9,23 @@ export default class PhFilterStep {
     constructor(dbstep) {
         this.content= dbstep
         this.expressions = JSON.parse(dbstep["expressions"])
+        this.command = this.instanceWithName()
+        // this.groupName = dbstep["group-name"]
+        // this.groupId = dbstep["group-id"]
+        // this.isGrouped = len(this.groupName) > 0
+    }
+
+    instanceWithName() {
+        switch (this.expressions.type) {
+        case "FilterOnValue":
+            return new PhFilterOnValueCmd(this.expressions)
+        default:
+            return null
+        }
     }
 
     exec() {
-
+        if (this.command.validations())
+            this.command.exec()
     }
 }
