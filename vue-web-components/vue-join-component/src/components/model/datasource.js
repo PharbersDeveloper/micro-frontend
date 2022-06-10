41f56646b12a8305093a12e9a1a3047a243864aa
alfred <yangyuanpig@gmail.com>
2022-06-10 12:13:58 +0800

    join

---
+++ vue-web-components/vue-join-component/src/components/model/datasource.js
@@ -0,0 +1,215 @@
+
+import { hostName } from "../../config/envConfig"
+import { JsonApiDataStore } from "jsonapi-datastore"
+
+
+export default class PhDataSource {
+    constructor(id) {
+        this.id = id
+        this.store = new JsonApiDataStore()
+        this.resetData()
+        this.debugToken = "67856ae8d1e8aaaedfb978a82dcdcfe4685c5f5871eb3d99549e1adb4a483f7b"
+    }
+
+    resetData() {
+        this.store.reset()
+        this.isReady = false
+        this.isMetaReady = false
+        this.hasNoSchema = false
+        this.step = null
+        this.dataset = null
+    }
+
+    getCookie(name) {
+        let arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)")
+        arr = document.cookie.match(reg)
+        if (arr) return (arr[2])
+        else return null
+    }
+
+    buildJobQuery(projectId, jobId) {
+        const url = `${hostName}/phdydatasource/query`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            "table": "dagconf",
+            "index_name": "dagconf-projectId-id-indexd",
+            "conditions": {
+                "projectId": ["=", projectId],
+                "id": ["=", jobId],
+            },
+            "limit": 1,
+            "start_key": {}
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    async queryJob(projectId, jobName) {
+        const that = this
+        await this.buildJobQuery(projectId, jobName)
+            .then((response) => response.json())
+            .then((response) => {
+                // console.log(response)
+                that.store.sync(response)
+                const data = that.store.findAll("dag-confs")
+                that.job = data[0]
+                that.datasets = JSON.parse(that.job["inputs"])
+            })
+    }
+
+    buildQuery(projectId, jobName) {
+        const url = `${hostName}/phdydatasource/query`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            "table": "step",
+            "conditions": {
+                // "pjName": ["=", this.projectId + "_" + this.jobName]
+                "pjName": ["=", projectId + "_" + jobName]
+            },
+            "limit": 1,
+            "start_key": {}
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    refreshData(projectId, jobName) {
+        const that = this
+        this.buildQuery(projectId, jobName)
+            .then((response) => response.json())
+            .then((response) => {
+                that.currentPageToken = response.meta.start_key
+                that.store.sync(response)
+                const data = that.store.findAll("steps").sort((l, r) => l["index"] - r["index"])
+                if (data.length === 0) {
+                    that.step = {
+                        pjName: "-".join(projectId, jobName),
+                        stepId: "1",
+                        ctype: "Join",
+                        expressions: JSON.stringify({
+                            "params": {
+                                "preFilters": [],
+                                "preJoinComputedColumns": [],
+                                "joins": [],
+                                "selectedColumns": [],
+                                "postJoinComputedColumns": [],
+                                "postFilter": {}
+                            }
+                        }),
+                        expressionsValue: "JSON",
+                        groupIndex: "0",
+                        groupName: "",
+                        id: "-".join(projectId, jobName, "1"),
+                        index: "1",
+                        runtime : "join",
+                        stepName: "join"
+                    }
+                } else {
+                    that.step = data[0]
+                }
+                that.isReady = true
+            })
+    }
+
+    buildDsMetaQuery(projectId, dsId) {
+        const url = `${hostName}/phdydatasource/query`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            "table": "dataset",
+            "conditions": {
+                "projectId": ["=", projectId],
+                "id": ["=", dsId]
+            },
+            "limit": 1,
+            "start_key": {}
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    refreshMateData(projectId, dsId) {
+        const that = this
+        this.buildDsMetaQuery(projectId, dsId)
+            .then((response) => response.json())
+            .then((response) => {
+                // that.currentPageToken = response.meta.start_key
+                that.store.sync(response)
+                const data = that.store.findAll("datasets")
+                that.dataset = data[0]
+                that.dataset.schema = JSON.parse(that.dataset["schema"])
+                if (that.dataset.schema.length === 0) {
+                    that.hasNoSchema = true
+                } else {
+                    that.isMetaReady = true
+                }
+            })
+    }
+
+    buildSaveQuery(projectId, jobName, param) {
+        // @wodelu 这里改成code gen 逻辑
+        const url = `${hostName}/phdydatasource/put_item`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            table: "step",
+            item: {
+                pjName: this.step["pj-name"],
+                stepId: this.step["step-id"],
+                ctype: this.step["ctype"],
+                expressions: JSON.stringify({ "parmas": param }),
+                expressionsValue: this.step["expressions-value"],
+                groupIndex: this.step["group-index"],
+                groupName: this.step["group-name"],
+                id: this.step["id"],
+                index: this.step["index"],
+                runtime : this.step["runtime"],
+                stepName: this.step["step-name"]
+            }
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    saveAndGenCode(projectId, jobName, parame) {
+        this.buildSaveQuery(projectId, jobName, parame)
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response)
+            })
+    }
+}
