fd4ae6869fd224b43af09c16f2113f38b383cfde
alfred <yangyuanpig@gmail.com>
2022-06-14 10:38:27 +0800

    group

---
+++ vue-web-components/vue-group-component/src/components/container.vue
@@ -0,0 +1,360 @@
+<template>
+    <div class="topn">
+        <link rel="stylesheet" href="https://components.pharbers.com/element-ui/element-ui.css">
+        <div class="topn_header">
+            <div class="header_left">
+                <img :src="defs.iconsByName('group')" alt="" />
+                <span>Group</span>
+            </div>
+            <div class="header_right">
+                <el-button class="save" @click="save">保存</el-button>
+            </div>
+        </div>
+        <div class="topn_area">
+            <div class="topn_left">
+                <el-steps direction="vertical" :active="active" align-center >
+                    <el-step v-for="(item, index) in stepsDefs" :key="index" :status="item.status">
+                        <template slot="title">
+                            <el-button type="text" @click="active = item.index" >{{item.title}}</el-button>
+                        </template>
+                    </el-step>
+                </el-steps>
+            </div>
+            <div class="topn_right" v-if="datasource.isReady && datasource.isMetaReady">
+                <pre-filter v-show="active === 1"
+                            ref="filter"
+                            :step="datasource.step"
+                            :schema="datasource.dataset.schema"
+                            @statusChange="preFilterStatus" />
+<!--                <computed v-show="active === 2"-->
+<!--                            ref="computed"-->
+<!--                            :step="datasource.step"-->
+<!--                            :schema="datasource.dataset.schema"-->
+<!--                            @statusChange="computedStatus" />-->
+<!--                <top-n v-show="active === 3"-->
+<!--                          ref="topn"-->
+<!--                          :step="datasource.step"-->
+<!--                          :schema="datasource.dataset.schema"-->
+<!--                          @statusChange="topnStatus" />-->
+<!--                <retrieved-cols v-show="active === 4"-->
+<!--                                ref="retrieved"-->
+<!--                                :step="datasource.step"-->
+<!--                                :schema="computedSchema"-->
+<!--                                @statusChange="retrievedStatus" />-->
+<!--                <outputs v-show="active === 5"-->
+<!--                                ref="outputs"-->
+<!--                                :schema="outputsSchema"-->
+<!--                                @statusChange="outputsStatus" />-->
+            </div>
+            <div v-if="datasource.hasNoSchema">
+                Schema 不对，找产品处理
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import PhDagDefinitions from './policy/definitions/definitions'
+import ElSteps from 'element-ui/packages/steps/index'
+import ElStep from 'element-ui/packages/step/index'
+import ElButton from 'element-ui/packages/button/index'
+import PhDataSource from './model/datasource'
+import PreFilter from './steps/commands/pre-filter/view'
+// import Computed from './steps/commands/computed/view'
+// import TopN from './steps/commands/top-n/view'
+// import RetrievedCols from './steps/commands/retrieved-cols/view'
+// import Outputs from './steps/commands/output/view'
+
+export default {
+    components: {
+        ElSteps,
+        ElStep,
+        ElButton,
+        PreFilter,
+        // Computed,
+        // TopN,
+        // RetrievedCols,
+        // Outputs
+    },
+    data() {
+        return {
+            computedSchema: [],
+            outputsSchema: [],
+            active: 1,
+            stepsDefs: [
+                {
+                    title: "Pre-Filter",
+                    index: 1,
+                    status: "wait"  // wait / process / finish / error / success
+                },
+                {
+                    title: "Computed Columns",
+                    index: 2,
+                    status: "wait"  // wait / process / finish / error / success
+                },
+                {
+                    title: "Group",
+                    index: 3,
+                    status: "wait"  // wait / process / finish / error / success
+                },
+                {
+                    title: "Custom aggregation",
+                    index: 4,
+                    status: "wait"  // wait / process / finish / error / success
+                },
+                {
+                    title: "Post Filter",
+                    index: 5,
+                    status: "wait"  // wait / process / finish / error / success
+                },
+                {
+                    title: "Outputs",
+                    index: 6,
+                    status: "wait"  // wait / process / finish / error / success
+                }
+            ]
+        }
+    },
+    props: {
+        allData: {
+            type: Object,
+            default: function() {
+                return {
+                    projectId: "YZYijD17N9L6LXx",
+                    projectName: "autorawdata2021",
+                    scriptsParams: null,
+                    dss: []
+                }
+            }
+        },
+        defs: {
+            type: Object,
+            default: function() {
+                return new PhDagDefinitions(1)
+            }
+        },
+        datasource: {
+            type: Object,
+            default: function() {
+                return new PhDataSource(1)
+            }
+        }
+    },
+    methods: {
+        getUrlParam(value) {
+            let href = window.location.href
+            let paramArr = href.split("?")[1].split("&")
+            let data = paramArr.find(item => item.indexOf(value) > -1)
+            return data ? decodeURI(data).split("=")[1] : undefined
+        },
+        getJobName() {
+            let jobShowName = this.getUrlParam("jobShowName") ? this.getUrlParam("jobShowName") : this.getUrlParam("jobName")
+            return [this.projectName, this.projectName, this.flowVersion, jobShowName].join("_")
+        },
+        preFilterStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[0].status = "success"
+            } else {
+                this.stepsDefs[0].status = "error"
+            }
+        },
+        computedStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[1].status = "success"
+            } else {
+                this.stepsDefs[1].status = "error"
+            }
+        },
+        groupStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[2].status = "success"
+            } else {
+                this.stepsDefs[2].status = "error"
+            }
+        },
+        customAggStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[2].status = "success"
+            } else {
+                this.stepsDefs[2].status = "error"
+            }
+        },
+        postFilterStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[3].status = "success"
+            } else {
+                this.stepsDefs[3].status = "error"
+            }
+        },
+        retrievedStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[3].status = "success"
+            } else {
+                this.stepsDefs[3].status = "error"
+            }
+        },
+        outputsStatus(status) {
+            // @wodelu 我只给你了写了一个状态的例子，这个逻辑是不对的
+            if (status) {
+                this.stepsDefs[4].status = "success"
+            } else {
+                this.stepsDefs[4].status = "error"
+            }
+        },
+        computeSchema() {
+            const result = []
+            for (let idx = 0; idx < this.datasource.dataset.schema.length; ++idx) {
+                result.push({
+                    "type": this.datasource.dataset.schema[idx]["type"].toLowerCase(),
+                    "title": this.datasource.dataset.schema[idx]["src"]
+                })
+            }
+            const addCols = this.$refs.computed.datasource.revert2Defs()
+            for (let idx = 0; idx < addCols.length; ++idx) {
+                result.push({
+                    "type": addCols[idx]["type"].toLowerCase(),
+                    "title": addCols[idx]["name"]
+                })
+            }
+            return result
+        },
+        genOutputsSchema() {
+            const retrieved = this.$refs.retrieved.datasource.revert2Defs()
+            let result = []
+            if (retrieved.length === 0) {
+                result = this.computedSchema
+            } else {
+                result = this.computedSchema.filter(x => retrieved.includes(x.title))
+            }
+            return result
+        },
+        save() {
+            const params = {
+                // "firstRows": this.$refs.topn.datasource.revert2Defs().firstRows,
+                // "lastRows": this.$refs.topn.datasource.revert2Defs().lastRows,
+                // "keys": this.$refs.topn.datasource.revert2Defs().keys,
+                // "preFilter": this.$refs.filter.datasource.revert2Defs(),
+                // "orders": this.$refs.topn.datasource.revert2Defs().orders,
+                // "denseRank": this.$refs.topn.datasource.revert2Defs().denseRank,
+                // "duplicateCount": this.$refs.topn.datasource.revert2Defs().duplicateCount,
+                // "rank": this.$refs.topn.datasource.revert2Defs().rank,
+                // "rowNumber": this.$refs.topn.datasource.revert2Defs().rowNumber,
+                // "retrievedColumns": this.$refs.retrieved.datasource.revert2Defs(),
+                // "computedColumns": this.$refs.computed.datasource.revert2Defs()
+            }
+
+            console.log(params)
+            // this.datasource.saveAndGenCode(this.projectIdTest, this.jobName, params)
+        },
+    },
+    mounted() {
+        this.projectId = this.getUrlParam("projectId")
+        this.projectName = this.getUrlParam("projectName")
+        this.projectIdTest = "alfredtest"
+        // this.jobName = this.getJobName()
+        this.jobName = "group"
+        // this.inputDsName = this.getUrlParam("inputName")
+        this.datasetId = this.getUrlParam("datasetId")
+        this.datasource.refreshData(this.projectIdTest, this.jobName)
+        this.datasource.refreshMateData(this.projectId, this.datasetId)
+    },
+    updated() {
+
+    },
+    watch: {
+        active(n) {
+            this.$refs.filter.validate()
+            this.$refs.computed.validate()
+            this.$refs.topn.validate()
+            this.$refs.retrieved.validate()
+            this.$refs.outputs.validate()
+
+            if (n === 4 || n === 5) {
+                this.computedSchema = this.computeSchema()
+            }
+
+            if (n === 5) {
+                this.outputsSchema = this.genOutputsSchema()
+            }
+        }
+    }
+}
+</script>
+<style lang="scss">
+    .topn {
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .op-factories {
+            // background: red;
+        }
+
+        .topn_header {
+            height: 48px;
+            padding: 0 15px;
+            border-bottom: 1px solid #cccccc;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .header_left {
+                display: flex;
+                align-items: center;
+
+                img {
+                    width: 30px;
+                    height: 30px;
+                    margin-right: 10px;
+                }
+
+                span {
+                    font-size: 20px;
+                    color: #000000;
+                    letter-spacing: 0.21px;
+                    font-weight: 600;
+                }
+            }
+
+            .header_right {
+                /*button {*/
+                /*    width: 65px;*/
+                /*    height: 26px;*/
+                /*    border: 1px solid #57565F;*/
+                /*    border-radius: 2px;*/
+                /*    background: none;*/
+                /*    cursor: pointer;*/
+                /*}*/
+            }
+        }
+
+        .topn_area {
+            width: 100%;
+            flex-grow: 1;
+            display: flex;
+            flex-direction: row;
+
+            .topn_left {
+                display: flex;
+                flex-direction: row;
+                margin-left: 80px;
+                justify-content: space-around;
+            }
+
+            .topn_right {
+                display: flex;
+                flex-grow: 1;
+                flex-direction: row;
+                justify-content: space-around;
+
+            }
+        }
+    }
+</style>
+
---
+++ vue-web-components/vue-group-component/src/components/steps/commands/pre-filter/view.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="pre-filter">
+        <link rel="stylesheet" href="https://components.pharbers.com/element-ui/element-ui.css">
+        <div class="condition-title">
+            <div class="condition-title-p">
+                <h2>Pre Filter</h2>
+                <div class="ver-center" v-if="datasource">
+                    <el-switch v-model="datasource.enabled" @change="$emit('statusChange', datasource.enabled)"></el-switch>
+                </div>
+            </div>
+            <el-form v-if="datasource">
+                <el-form-item label="保留符合条件的列">
+                    <select v-model="datasource.command.action">
+                        <option v-for="(item, index) in concretDefs.actions" :value="item.cal" :key="index" :label="item.desc" />
+                    </select>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="condition-selection" v-if="datasource">
+            <div class="condition-selection-item" v-for="(cur, index) in datasource.command.cloases" :key="index">
+                <div class="condition-selection-content">
+                    <select v-model="cur['left']">
+                         <option v-for="(item, index) in schema" :value="item.src" :key="index" :label="item.src" />
+                    </select>
+                    <select v-model="cur['op']">
+                        <option v-for="(item, index) in concretDefs.includes" :value="item.cal" :key="index" :label="item.desc" />
+                    </select>
+                    <el-input v-if="cur['op'] !== 'EXISTS' && cur['op'] !== 'NOT-EXISTS'" v-model="cur['right']" ></el-input>
+                    <el-input v-else disabled ></el-input>
+                </div>
+                <el-button type="text" @click="datasource.command.delcloases(index)">删除</el-button>
+            </div>
+        </div>
+        <div class="condition-add-button">
+            <el-button type="primary" @click="datasource.command.insertcloases()">添加</el-button>
+        </div>
+    </div>
+</template>
+<script>
+import { PhPreFilterDefs } from "./defs"
+import ElForm from 'element-ui/packages/form/index'
+import ElFormItem from 'element-ui/packages/form-item/index'
+import ElInput from 'element-ui/packages/input/index'
+import ElButton from 'element-ui/packages/button/index'
+import ElSwitch from 'element-ui/packages/switch/index'
+import PhFilterStep from "./step"
+
+export default {
+    data() {
+        return {
+            datasource: null
+        }
+    },
+    props: {
+        step: Object,
+        schema: Array,
+        concretDefs: {
+            type: Object,
+            default: () => {
+                return PhPreFilterDefs
+            }
+        }
+    },
+    components: {
+        ElFormItem,
+        ElForm,
+        ElInput,
+        ElButton,
+        ElSwitch
+    },
+    // updated() {
+    mounted() {
+        this.datasource = new PhFilterStep(this.step)
+        this.$emit('statusChange', this.datasource.enabled)
+    },
+    methods: {
+        validate() {
+            this.$emit('statusChange', this.datasource.enabled)
+        }
+    },
+    computed: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    * {
+        letter-spacing: .4px;
+        line-height: 1.6;
+        box-sizing: border-box;
+    }
+    .pre-filter {
+        margin-top: 4px;
+        /*width: 100%;*/
+        min-width: 800px;
+        padding: 4px;
+        display: flex;
+        flex-direction: column;
+
+        .condition-title {
+            display: flex;
+            flex-direction: column;
+
+            .condition-title-p {
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+
+                .ver-center {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-around;
+                }
+            }
+        }
+
+        .condition-selection {
+            margin-top: 30px;
+
+            .condition-selection-item {
+                display: flex;
+                flex-direction: row;
+
+            }
+
+            .condition-selection-content {
+                display: flex;
+                flex-direction: row;
+
+            }
+        }
+
+        .condition-add-button {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            margin-top: 80px;
+        }
+    }
+</style>
