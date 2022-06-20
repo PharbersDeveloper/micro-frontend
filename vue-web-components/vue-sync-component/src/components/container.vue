1f0baac4bc4dd851cded9d0ddf71d8250deeb403
wodelu <1058744843@qq.com>
2022-06-20 11:42:58 +0800

    sync

--- vue-web-components/vue-sync-component/src/components/container.vue
+++ vue-web-components/vue-sync-component/src/components/container.vue
@@ -3,7 +3,6 @@
         <link rel="stylesheet" href="https://components.pharbers.com/element-ui/element-ui.css">
         <div class="sync_header">
             <div class="header_left">
-                <!-- 需要一个sync 的icon @wodelu -->
                 <img :src="defs.iconsByName('prepare')" alt="" />
                 <span>Sync</span>
             </div>
@@ -13,31 +12,70 @@
         </div>
         <div class="sync_area">
             <div class="sync_left">
-                <div class="content">
+                <div class="content"  v-show="!selectInput">
                     <div class="content-column">
                         <span>Input</span>
                         <el-input readonly v-model="inputDsName"></el-input>
-                        <el-button type="primary" @click="changeInputDsName" disabled>更改数据集</el-button>
-<!--                        <span class="content-bottom">Version Selection</span>-->
-<!--                        <div class="version-tags-container">-->
-
-<!--                        </div>-->
-<!--                        <el-input v-model="search"></el-input>-->
-<!--                        <div class="version-ds-list">-->
-
-<!--                        </div>-->
+                        <el-button type="primary" @click="selectInput=!selectInput">更改数据集</el-button>
                     </div>
                 </div>
+				<div class="input-for-select" v-show="selectInput">
+					<div class="search">
+						<el-form label-width="60px">
+							<el-form-item label="搜索">
+								<el-input
+									placeholder="搜索"
+									v-model="searchInputName"></el-input>
+							</el-form-item>
+						</el-form>
+						<i 
+							@click="selectInput=!selectInput"
+							class="el-icon-close pointer"></i>
+					</div>
+					<ul class="list">
+						<li 
+							@click="selectInputItem(item)"
+							class="addInput pointer" 
+							:key="item+'input'" 
+							v-for="item in inArray">
+							<i class="el-icon-plus pointer mr-4"></i>
+							{{item}}
+						</li>
+					</ul>
+				</div>
             </div>
             <div class="sync_divider"></div>
             <div class="sync_right">
-                <div class="content">
+                <div class="content" v-show="!selectOutput">
                     <div class="content-column">
                         <span>Output</span>
                         <el-input readonly v-model="outputDsName"></el-input>
-                        <el-button type="primary" @click="changeOutputDsName" disabled>更改数据集</el-button>
+                        <el-button type="primary" @click="selectOutput=!selectOutput">更改数据集</el-button>
                     </div>
                 </div>
+				<div class="input-for-select" v-show="selectOutput">
+					<div class="search">
+						<el-form label-width="60px">
+							<el-form-item label="搜索">
+								<el-input
+									placeholder="搜索"
+									v-model="searchInputName"></el-input>
+							</el-form-item>
+						</el-form>
+						<i class="el-icon-close pointer" 			
+							@click="selectOutput=!selectOutput"></i>
+					</div>
+					<ul class="list">
+						<li 
+							@click="selectOutputItem(item)"
+							class="addInput pointer" 
+							:key="item+'output'" 
+							v-for="item in outArray">
+							<i class="el-icon-plus pointer mr-4"></i>
+							{{item}}
+						</li>
+					</ul>
+				</div>
             </div>
         </div>
     </div>
@@ -55,8 +93,12 @@ export default {
     data() {
         return {
             inputDsName: this.getUrlParam("inputName"),
-            outputDsName: this.getUrlParam("outputName")
-            // schema: ["1", "2", "3"],
+            outputDsName: this.getUrlParam("outputName"),
+            selectInput: false,
+            selectOutput: false,
+            outputsArray: [],
+            inputsArray: [],
+            searchInputName: ""
         }
     },
     props: {
@@ -76,7 +118,11 @@ export default {
             default: function() {
                 return new PhDagDefinitions(1)
             }
-        }
+        },
+        outputs: Array,
+        inputs: Array,
+        inArray: Array,
+        outArray: Array
     },
     methods: {
         getUrlParam(value) {
@@ -115,10 +161,16 @@ export default {
         this.projectName = this.getUrlParam("projectName")
         this.jobName = this.getJobName()
         this.inputDsName = this.getUrlParam("inputName")
+    },
+
+    watch: {
+        "inputs": function(n) {
+            this.inputsArray = n
+        },
+		"outputs": function(n) {
+            this.outputsArray = n
+		}
     }
-    // updated() {
-    //     this.outputDsName = this.allData.outputs[0]
-    // }
 }
 </script>
 <style lang="scss">
@@ -126,7 +178,22 @@ export default {
         box-sizing: border-box;
         display: flex;
         flex-direction: column;
-        height: 100%;
+        height: 100%;\
+
+		.pointer {
+			cursor: pointer;
+		}
+		.left {
+			flex: 1;
+			padding: 20px;
+		}
+		.right {
+			flex: 1;
+			padding: 20px;
+		}
+		.title {
+			margin-bottom: 10px;
+		}
 
         .op-factories {
             // background: red;
@@ -171,16 +238,55 @@ export default {
         }
 
         .sync_area {
-            width: 100%;
+            // width: 100%;
             flex-grow: 1;
             display: flex;
             flex-direction: row;
+			height: calc(100vh - 100px);
+			background: #f2f2f2;
+			padding: 40px;
+
+			.input-for-select {
+				width: 100%;
+				.search {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					padding-bottom: 10px;
+					border-bottom: 1px solid #ccc;
+
+					.el-form-item {
+						margin-bottom: 0;
+					}
+				}
+
+				.list {
+					padding: 0;
+					margin: 0;
+					font-size: 15px;
+					color: #666666;
+
+					.addInput {
+						list-style: none;
+						height: 41px;
+						line-height: 41px;
+						padding: 0px 28px 0px 28px;
+						box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+						border-bottom: 1px solid #ccc;
+					}
+
+					.mr-4 {
+						margin-right: 4px;
+					}
+				}
+			}
 
             .sync_left {
                 flex-grow: 1;
                 display: flex;
                 flex-direction: row;
                 justify-content: space-around;
+				padding: 40px;
             }
 
             .sync_divider {
@@ -192,6 +298,7 @@ export default {
                 display: flex;
                 flex-direction: row;
                 justify-content: space-around;
+				padding: 40px;
 
             }
 
---
+++ vue-web-components/vue-sync-component/src/components/model/datasource.js
@@ -0,0 +1,291 @@
+
+import { hostName } from "../../config/envConfig"
+import { JsonApiDataStore } from "jsonapi-datastore"
+
+
+export default class PhDataSource {
+    constructor(id, parent) {
+        this.id = id
+		this.parent = parent
+        this.store = new JsonApiDataStore()
+        this.resetData()
+        this.debugToken = "27a68748c55abfadcab0a85e012b1c73f7380ed319cf734a6cb179d45b1ea24d"
+    }
+
+    resetData() {
+        this.store.reset()
+        this.isReady = false
+        this.isMetaReady = false
+        this.hasNoSchema = false
+        this.step = null
+        this.dataset = null
+    }
+
+    getCookie(name) {
+        let arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)")
+        arr = document.cookie.match(reg)
+        if (arr) return (arr[2])
+        else return null
+    }
+
+    buildQuery(projectId, jobName) {
+        const url = `${hostName}/phdydatasource/query`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            "table": "step",
+            "conditions": {
+                // "pjName": ["=", this.projectId + "_" + this.jobName]
+                "pjName": ["=", projectId + "_" + jobName]
+            },
+            "limit": 1,
+            "start_key": {}
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    refreshData(projectId, jobName) {
+        const that = this
+        this.buildQuery(projectId, jobName)
+            .then((response) => response.json())
+            .then((response) => {
+                that.currentPageToken = response.meta.start_key
+                that.store.sync(response)
+                const data = that.store.findAll("steps").sort((l, r) => l["index"] - r["index"])
+                if (data.length === 0) {
+                    that.step = {
+                        "pj-name": [projectId, jobName].join("_"),
+                        "step-id": "1",
+                        ctype: "Sort",
+                        expressions: JSON.stringify({
+                            "params": {
+                                "preFilter": {
+                                    "distinct": false,
+                                    "enabled": true,
+                                    "expression": ""
+                                },
+                                "orders": [],
+                                "denseRank": false,
+                                "rank": false,
+                                "rowNumber": false,
+                                "computedColumns": [
+									{
+										"expr": "",
+										"name": "",
+										"type": "int"
+									}
+								]
+                            }
+                        }),
+                        "expressions-value": "JSON",
+                        "group-index": "0",
+                        "group-name": "",
+                        id: [projectId, jobName, "1"].join("_"),
+                        index: "1",
+                        runtime : "sort",
+                        "step-name": "sort"
+                    }
+                } else {
+                    that.step = data[0]
+                }
+                that.isReady = true
+            })
+    }
+
+    // buildDsMetaQuery(projectId, dsId) {
+    //     const url = `${hostName}/phdydatasource/query`
+    //     const accessToken = this.getCookie( "access_token" ) || this.debugToken
+    //     let body = {
+    //         "table": "dataset",
+    //         "conditions": {
+    //             "projectId": ["=", projectId],
+    //             "id": ["=", dsId]
+    //         },
+    //         "limit": 1,
+    //         "start_key": {}
+    //     }
+
+    //     let options = {
+    //         method: "POST",
+    //         headers: {
+    //             "Authorization": accessToken,
+    //             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    //             "accept": "application/json"
+    //         },
+    //         body: JSON.stringify(body)
+    //     }
+    //     return fetch(url, options)
+    // }
+
+    // refreshMateData(projectId, dsId) {
+    //     const that = this
+    //     this.buildDsMetaQuery(projectId, dsId)
+    //         .then((response) => response.json())
+    //         .then((response) => {
+    //             // that.currentPageToken = response.meta.start_key
+    //             that.store.sync(response)
+    //             const data = that.store.findAll("datasets")
+    //             that.dataset = data[0]
+    //             that.dataset.schema = JSON.parse(that.dataset["schema"])
+    //             if (that.dataset.schema.length === 0) {
+    //                 that.hasNoSchema = true
+    //             } else {
+    //                 that.isMetaReady = true
+    //             }
+    //         })
+    // }
+
+	refreshDataset(projectId, dsId) {
+        const that = this
+        this.buildDatasetQuery(projectId)
+            .then((response) => response.json())
+            .then((response) => {
+				console.log(response)
+				that.store.sync(response)
+                const data = that.store.findAll("datasets")
+				that.parent.datasetArray = data
+                that.dataset = data.filter(it => it.id === dsId)[0]
+                that.dataset.schema = JSON.parse(that.dataset["schema"])
+				if (that.dataset.schema.length === 0) {
+                    that.hasNoSchema = true
+                } else {
+                    that.isMetaReady = true
+                }
+            })
+    }
+
+	buildDatasetQuery(projectId) {
+        const url = `${hostName}/phdydatasource/query`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+            "table": "dataset",
+            "conditions": {
+                "projectId": ["=", projectId]
+            },
+			index_name: "dataset-projectId-name-index",
+            "limit": 1000,
+            "start_key": {}
+        }
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+	refreshInOut(projectId, jobName) {
+        const that = this
+        this.buildInOutQuery(projectId, jobName)
+            .then((response) => response.json())
+            .then((response) => {
+				console.log(response)
+				that.parent.inArray = response.input
+				that.parent.outArray = response.output
+            })
+    }
+
+	buildInOutQuery(projectId, jobName) {
+        const url = `${hostName}/phcheckinout`
+        const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        let body = {
+			"name": jobName,
+			"projectId": projectId
+		}
+
+        let options = {
+            method: "POST",
+            headers: {
+                "Authorization": accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                "accept": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+    }
+
+    buildSaveQuery(projectId, jobName, param) {
+		const steps = [{
+			pjName: this.step["pj-name"],
+			stepId: this.step["step-id"],
+			ctype: this.step["ctype"],
+			expressions: {
+				"type": "sort",
+				"code": "pyspark",
+				"params": param
+			},
+			expressionsValue: this.step["expressions-value"],
+			groupIndex: this.step["group-index"],
+			groupName: this.step["group-name"],
+			id: this.step["id"],
+			index: this.step["index"],
+			runtime : this.step["runtime"],
+			stepName: this.step["step-name"]
+		}]
+		const event = new Event("event")
+		event.args = {
+			callback: "saveSort",
+			element: this,
+			param: {
+				name: "saveSort",
+				projectId: this.parent.projectId,
+				projectName: this.parent.projectName,
+				stepsArr: steps
+			}
+		}
+		this.parent.$emit('event', event)
+        // @wodelu 这里改成code gen 逻辑
+        // const url = `${hostName}/phdydatasource/put_item`
+        // const accessToken = this.getCookie( "access_token" ) || this.debugToken
+        // let body = {
+        //     table: "step",
+        //     item: {
+        //         pjName: this.step["pj-name"],
+        //         stepId: this.step["step-id"],
+        //         ctype: this.step["ctype"],
+        //         expressions: JSON.stringify({ "params": param }),
+        //         expressionsValue: this.step["expressions-value"],
+        //         groupIndex: this.step["group-index"],
+        //         groupName: this.step["group-name"],
+        //         id: this.step["id"],
+        //         index: this.step["index"],
+        //         runtime : this.step["runtime"],
+        //         stepName: this.step["step-name"]
+        //     }
+        // }
+
+        // let options = {
+        //     method: "POST",
+        //     headers: {
+        //         "Authorization": accessToken,
+        //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        //         "accept": "application/json"
+        //     },
+        //     body: JSON.stringify(body)
+        // }
+        // return fetch(url, options)
+    }
+
+    saveAndGenCode(projectId, jobName, parame) {
+        this.buildSaveQuery(projectId, jobName, parame)
+            // .then((response) => response.json())
+            // .then((response) => {
+            //     console.log(response)
+            // })
+    }
+}
