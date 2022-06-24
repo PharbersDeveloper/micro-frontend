81c64ae60531c66908024db9b4dd085515f76253
alfred <yangyuanpig@gmail.com>
2022-06-24 17:08:43 +0800

    深copy

--- web-shell/app/routes/shell.js
+++ web-shell/app/routes/shell.js
@@ -8,11 +8,11 @@ export default class ShellRoute extends Route {
 	@service oauthService
 	@service cookies
 	@service store
-    @service browserEventsService;
+	@service browserEventsService
 	@service("remote-loading") jsl
 	@service("route-parse") rps
 	@service("ph-menu") ms
-	@service('loading') loadingService
+	@service("loading") loadingService
 	@service("execution-status") noticeService
 
 	get auth() {
@@ -23,7 +23,7 @@ export default class ShellRoute extends Route {
 		return ENV.environment
 	}
 
-	beforeModel(transition){	
+	beforeModel(transition) {
 		if (this.auth && !this.oauthService.judgeAuth()) {
 			alert("登录过期，请重新登录")
 			this.oauthService.obtainAuth()
@@ -47,7 +47,7 @@ export default class ShellRoute extends Route {
 			const tmp = pages[idx]
 			let param = window.location.href.split("?")[1]
 			let routeName = params.path
-			if(param && !params.path.split("?")[1]) {
+			if (param && !params.path.split("?")[1]) {
 				routeName = `${params.path}?${param}`
 			}
 			const [match, result] = this.rps.parse("/" + routeName, tmp.route)
@@ -61,17 +61,57 @@ export default class ShellRoute extends Route {
 		/**
 		 * 2. 动态的把需要的JS加载到dom中
 		 */
-		if (curPage.uri)
-			await this.jsl.loadRemoteJs(curPage.uri)
-		if (curPage.cat)
-			await this.jsl.loadRemoteJsSync(curPage.cat)
+		if (curPage.uri) await this.jsl.loadRemoteJs(curPage.uri)
+		if (curPage.cat) await this.jsl.loadRemoteJsSync(curPage.cat)
 		const clientName = curPage.clientName
 		const modelName = camelize(curPage.name) + "RouteModel"
-		const data = await window[clientName][modelName](this, parseParams, curPage)
+		const data = await window[clientName][modelName](
+			this,
+			parseParams,
+			curPage
+		)
+
+		if (data) {
+			const tmp = this.deepCopy(data)
+			console.log("alfredtest")
+			console.log(tmp)
+		}
+
 		return RSVP.hash({
 			page: curPage,
 			data: data ? data : {},
-			_isVue: true
+			// _isVue: true
 		})
+	}
+
+	deepCopy(obj, cache = []) {
+		function find(list, f) {
+			return list.filter(f)[0]
+		}
+
+		// just return if obj is immutable value
+		if (obj === null || typeof obj !== "object") {
+			return obj
+		}
+
+		// if obj is hit, it is in circular structure
+		const hit = find(cache, (c) => c.original === obj)
+		if (hit) {
+			return hit.copy
+		}
+
+		const copy = Array.isArray(obj) ? [] : {}
+		// put the copy into cache at first
+		// because we want to refer it in recursive deepCopy
+		cache.push({
+			original: obj,
+			copy
+		})
+
+		Object.keys(obj).forEach((key) => {
+			copy[key] = this.deepCopy(obj[key], cache)
+		})
+
+		return copy
 	}
 }
