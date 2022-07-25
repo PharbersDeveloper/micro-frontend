14c2799db6fb576dd3a2dcd8640e99a8b1d53860
wodelu <1058744843@qq.com>
2022-07-25 14:02:47 +0800

    jira号: 3071， fix: sort prefilter

--- vue-web-components/vue-sort-component/src/components/steps/commands/pre-filter/step.js
+++ vue-web-components/vue-sort-component/src/components/steps/commands/pre-filter/step.js
@@ -4,15 +4,14 @@ import PhPreFilterCmd from "./cmd"
  * 这个就是我所说的Command
  */
 export default class PhFilterStep {
-    constructor(dbstep) {
+    constructor(dbstep, schema) {
         this.content= dbstep
         this.expressions = JSON.parse(dbstep["expressions"])
         const defs = this.expressions["params"]["preFilter"]
         this.distinct = defs["distinct"]
         this.enabled = defs["enabled"]
         this.preFilterExpression = defs["expression"]
-        this.command = new PhPreFilterCmd(this.preFilterExpression)
-        console.log(this.command.cloases)
+        this.command = new PhPreFilterCmd(this.preFilterExpression, schema)
     }
 
     exec() {
