3ab0eba0c003d67db804f4375998deaf849df993
Alex <891366402@qq.com>
2022-08-16 12:04:14 +0800

    打包报错，璐姐求救

--- vue-web-components/vue-topn-component/src/components/container.vue
+++ vue-web-components/vue-topn-component/src/components/container.vue
@@ -150,7 +150,6 @@ export default {
             active: 3,
             flowVersion: "developer",
             activeName: "Setting",
-            activeNameOld: "",
             datasetArray: [],
             stepsDefs: [
                 {
