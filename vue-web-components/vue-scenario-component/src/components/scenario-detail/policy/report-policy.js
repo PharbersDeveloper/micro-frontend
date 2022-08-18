ff5c8094576663420ce5920ef43b407ee7561c11
hfzhang <zhf13937061458@163.com>
2022-08-18 19:20:18 +0800

    jira号:3314 fix: 修复report接口，step样式

--- vue-web-components/vue-scenario-component/src/components/scenario-detail/policy/report-policy.js
+++ vue-web-components/vue-scenario-component/src/components/scenario-detail/policy/report-policy.js
@@ -22,17 +22,14 @@ export default class PhScenarioReportPolicy {
 		reportArray.forEach(report => {
 			let detail = {}
 			detail = {
-                // "timezone": report.timezone,
                 "type": report.type,
-                // "start": report.start,
-                // "period": report.period,
                 "destination": report.destination
             }
 			arr.push({
 				scenarioId: report.scenarioId,
 				id: report.id,
 				index: report.index,
-				// active: report.active,
+				active: report.active,
 				detail: detail,
 				// mode: report.mode,
 				traceId: report.traceId,
